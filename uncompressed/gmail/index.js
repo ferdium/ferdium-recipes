d7d80c0fbbe440f8f142327474f56c74e2df6c4a
Amine Mouafik <amine@mouafik.fr>
2019-11-27 18:01:43 +0700

    Override user agent in Gmail recipe

--- uncompressed/gmail/index.js
+++ uncompressed/gmail/index.js
@@ -1,2 +1,7 @@
-// just pass through Franz
-module.exports = Franz => Franz;
+module.exports = Franz =>
+  class Instagram extends Franz {
+    // Fixes
+    overrideUserAgent() {
+      return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
+    }
+  };
