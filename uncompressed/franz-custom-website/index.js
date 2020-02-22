f7422c3a9a02553991e9b96d03b76aed1173e71c
Amine Mouafik <amine@mouafik.fr>
2020-02-22 21:34:12 +0100

    Validate all https responses different from 4xx

--- uncompressed/franz-custom-website/index.js
+++ uncompressed/franz-custom-website/index.js
@@ -6,7 +6,7 @@ module.exports = Franz => class CustomWebsite extends Franz {
       const resp = await window.fetch(url, {
         method: 'GET'
       });
-      return resp.status.toString().startsWith('2');
+      return !resp.status.toString().startsWith('4');
     } catch (err) {
       console.error(err);
     }
