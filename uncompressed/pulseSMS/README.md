bc41498c61867ebe7ce00a0cb6fa82e74627bfd1
vantezzen <hello@vantezzen.io>
2020-03-08 10:23:32 +0100

    Fix differences in recipes

--- uncompressed/pulseSMS/README.md
+++
@@ -1,7 +0,0 @@
-# PulseSMS for Franz
-This is a Franz recipe for PulseSMS web client
-
-[Pulsesms.app](https://pulsesms.app)
-
-### How to create your own Franz recipes:
-* [Read the documentation](https://github.com/meetfranz/plugins)
--- uncompressed/pulseSMS/webview.js
+++
@@ -1,8 +0,0 @@
-module.exports = (Franz) => {
-  function getMessages() {
-    Franz.setBadge(document.querySelector("#unread_count").innerHTML.replace(/\s/g,''));
-  }
-
-  // check for new messages every second and update Franz badge
-  Franz.loop(getMessages);
-};
