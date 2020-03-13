21bebcc1f8862b49f9750ba8b4010192ccd9e51c
vantezzen <hello@vantezzen.io>
2020-03-13 09:22:50 +0100

    #62 Add Zoho recipe (@jaredkotoff)

---
+++ uncompressed/zoho/README.md
@@ -0,0 +1,6 @@
+# Zoho Mail for Franz 5
+from plugins legacy 
+[meetfranz](https://github.com/meetfranz/franz)
+
+### How to create your own Franz recipes:
+* [Read the documentation](https://github.com/meetfranz/plugins)
---
+++ uncompressed/zoho/webview.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = (Franz, options) => {
+  const getMessages = () => {
+    const unreadMail = $(".zm_urd").not(".hidden").length;
+
+    Franz.setBadge(unreadMail);
+  }
+
+  Franz.loop(getMessages);
+};
