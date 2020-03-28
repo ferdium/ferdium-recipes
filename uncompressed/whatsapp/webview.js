2dd9fe8b06468580d75f0948478cd8e10732b76f
vantezzen <hello@vantezzen.io>
2020-03-28 10:06:50 +0100

    Update Whatsapp

--- uncompressed/whatsapp/webview.js
+++ uncompressed/whatsapp/webview.js
@@ -39,7 +39,7 @@ module.exports = Franz => {
     let count = 0;
 
     for (let i = 0; i < elements.length; i += 1) {
-      if (elements[i].querySelectorAll('.P6z4j').length === 1 && elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
+      if (elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
         count += 1;
       }
     }
