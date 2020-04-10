7847288c8904258b92b78a6811a8b915bcf8d14e
Jake Lee <github@jakelee.co.uk>
2020-04-10 23:24:57 +0100

    Replaces tab indents with spaces

--- uncompressed/nextdoor/webview.js
+++ uncompressed/nextdoor/webview.js
@@ -2,7 +2,7 @@
 
 module.exports = Franz => {
   const getMessages = function getMessages() {
-	const unread = document.getElementsByClassName('notification-badge')[0].innerText;
+    const unread = document.getElementsByClassName('notification-badge')[0].innerText;
     Franz.setBadge(parseInt(unread, 10));
   };
 
