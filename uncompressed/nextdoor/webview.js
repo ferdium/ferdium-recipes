9c9ebafaf4af21736a48bec17703b14d5302d352
Jake Lee <github@jakelee.co.uk>
2020-04-10 23:12:48 +0100

    Add Nextdoor recipe

---
+++ uncompressed/nextdoor/webview.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = Franz => {
+  const getMessages = function getMessages() {
+	const unread = document.getElementsByClassName('notification-badge')[0].innerText;
+    Franz.setBadge(parseInt(unread, 10));
+  };
+
+  Franz.loop(getMessages);
+};
