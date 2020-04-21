0f49db845a6b697739357beec7825714e56f252c
Feiko Joosten <feiko_joosten@hotmail.com>
2020-04-21 21:35:57 +0200

    Adds notifications for GitHub (#129)
    * Added notifications for github.
    
    * Changed the notification count to use indirect notifications
    
    Changed the notification count to use indirect notifications instead of direct notifications

--- uncompressed/github/webview.js
+++ uncompressed/github/webview.js
@@ -1,2 +1,10 @@
 "use strict";
+
+module.exports = Franz => {
+  const getMessages = function getMessages() {
+    Franz.setBadge(document.querySelector('[class*="mail-status unread"]') !== null ? 1 : 0);
+  };
+
+  Franz.loop(0, getMessages);
+}
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJnaXRodWIvd2Vidmlldy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
