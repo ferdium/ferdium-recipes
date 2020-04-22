911d1ded846165a4b420efa2b58668ad27c9f2c7
Amine <amine@mouafik.fr>
2020-04-22 07:13:51 +0000

    Use GitHub notifications center and direct notifications (#133)

--- uncompressed/github/webview.js
+++ uncompressed/github/webview.js
@@ -2,9 +2,19 @@
 
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    Franz.setBadge(document.querySelector('[class*="mail-status unread"]') !== null ? 1 : 0);
+    const directCountElement = document.querySelector('.filter-list .count');
+    const indirectCountElement = document.querySelector('[class*="mail-status unread"]')
+    let directCount, indirectCount
+    if (directCountElement) {
+      directCount = parseInt(directCountElement.innerHTML, 10);
+    } else {
+      if (indirectCountElement) {
+        indirectCount = 1;
+      }
+    }
+    Franz.setBadge(directCount, indirectCount);
   };
 
-  Franz.loop(0, getMessages);
+  Franz.loop(getMessages);
 }
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJnaXRodWIvd2Vidmlldy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
