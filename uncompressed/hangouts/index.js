33e211fa2494de716a3514cde492333b2aa444d5
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2020-04-27 20:02:54 +0000

    Pasting text from clipboard into hangouts prepends it (#142)

--- uncompressed/hangouts/index.js
+++ uncompressed/hangouts/index.js
@@ -1,11 +1,15 @@
-var os = require('os')
-
-module.exports = Franz =>
+module.exports = (Franz) =>
   class hangouts extends Franz {
-    overrideUserAgent() {
-      if (os.platform() == 'linux')
-        return "Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
-      else
-        return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0";
+    modifyRequestHeaders() {
+      return [
+        {
+          headers: {
+            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g, '').trim(),
+          },
+          requestFilters: {
+            urls: ['*://*/*'],
+          },
+        },
+      ];
     }
   };
