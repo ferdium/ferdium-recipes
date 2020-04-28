d8b7b63dcaab46940a271939b7284100b7ac9328
RoiArthurB <contact@arthurbrugiere.fr>
2020-04-28 17:43:27 +0700

    [ZALO] Fix variable use Thanks @vantezzen

--- uncompressed/zalo/webview.js
+++ uncompressed/zalo/webview.js
@@ -4,7 +4,7 @@ module.exports = Franz => {
   const getMessages = function getMessages() {
     var unread = 0
     const notificationBadge = document.getElementsByClassName('tab-red-dot').length;
-    Franz.setBadge(unread);
+    Franz.setBadge(notificationBadge);
   };
 
   Franz.loop(getMessages);
