5ad19c79dc5747d70080eba99c2d95487dac9521
Daniele Lenares <daniele.lenares@gmail.com>
2020-05-06 17:31:06 +0200

    Bump Clubhouse to 1.0.1

--- uncompressed/clubhouse/webview.js
+++ uncompressed/clubhouse/webview.js
@@ -1,9 +1,8 @@
 module.exports = (Ferdi) => {
   function getMessages() {
-    let unread = 0;
     const hasNotifications = document.querySelector('#notifications-link .badge').classList.contains('visible');
     if (hasNotifications) {
-      Ferdi.setBadge(1);
+      Ferdi.setBadge(0, 1);
     }
   }
 
