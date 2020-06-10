23a073ab74fa0b4265c0f5569c90c100605e6fb2
Danny Qiu <dqiu55@gmail.com>
2020-06-10 00:03:44 -0700

    Fix default link handling in Skype recipe

--- uncompressed/skype/webview.js
+++ uncompressed/skype/webview.js
@@ -88,10 +88,10 @@ module.exports = (Franz, settings) => {
 
     if (link || button) {
       const url = link ? link.getAttribute('href') : button.getAttribute('title');
-      event.preventDefault();
-      event.stopPropagation();
 
       if (url.includes('views/imgpsh_fullsize_anim')) {
+        event.preventDefault();
+        event.stopPropagation();
         let win = new BrowserWindow({
           width: 800,
           height: window.innerHeight,
@@ -101,11 +101,6 @@ module.exports = (Franz, settings) => {
           }
         });
         win.loadURL(url);
-        win.on('closed', () => {
-          win = null;
-        });
-      } else {
-        window.open(url);
       }
     }
   }, true);
