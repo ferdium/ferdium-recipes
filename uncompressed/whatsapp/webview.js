7b6f329b85912afab5d8576de26ed57f25230131
Rui Gonçalves <rui@fb.com>
2020-07-20 13:23:23 +0100

    Add support for empty unread badges
    Empty unread badges show up when people manually "Mark as Unread" chat threads. Currently, this stops notifications from appearing as counts become NaN. With this PR they will stop being counted (since we don't have message counts in those cases and it's not really a notification I think this is probably the best behavior, but I'm ok with defaulting to 1).

--- uncompressed/whatsapp/webview.js
+++ uncompressed/whatsapp/webview.js
@@ -33,7 +33,7 @@ module.exports = Franz => {
 	var indirectCount = 0;
 
     for (var i = 0; i < elements.length; i += 1) {
-	  var countValue = parseInt(elements[i].textContent, 10);
+	  var countValue = parseInt(elements[i].textContent || '0', 10);
 	  
       if (elements[i].parentNode.previousElementSibling === null || elements[i].parentNode.previousElementSibling.querySelectorAll("[data-icon=muted]").length === 0) {
         count += countValue;
