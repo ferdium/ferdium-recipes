3ba70d1d471004ccb7fd09ed217cea41b6335235
Roman Dąbal <dabalroman@gmail.com>
2020-09-19 20:47:19 +0200

    Add Pomodoro Tracker recipe (#252)

---
+++ uncompressed/pomodoro-tracker/index.js
@@ -0,0 +1,2 @@
+module.exports = Ferdi => class pomodorotracker extends Ferdi {
+};
---
+++ uncompressed/pomodoro-tracker/webview.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _path = _interopRequireDefault(require("path"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = Ferdi => {
+  const getMessages = function getMessages() {
+    Ferdi.setBadge(0, 0);
+  };
+
+  Ferdi.loop(getMessages);
+  Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
