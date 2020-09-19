762edbc0c72dd53f4f50366a02c24b4d048f5e88
Patrick Kastner <patrick.kastner@gmail.com>
2020-09-19 15:09:00 -0400

    Add Feedly recipe (#254)
    Co-authored-by: Amine <amine@mouafik.fr>

---
+++ uncompressed/feedly/README.md
@@ -0,0 +1,2 @@
+# Ferdi 5 Recipe: Feedly
+This recipe integrates Feedly into Ferdi.
---
+++ uncompressed/feedly/webview.js
@@ -0,0 +1,16 @@
+module.exports = Franz => {
+
+  getMessages = () => {
+    const newsDOM = document.querySelectorAll("div[title='All'] > .LeftnavListRow__count")[0].innerHTML;
+    let counter = parseInt(newsDOM);
+    
+    if (newsDOM.indexOf('K') !== -1 || newsDOM.indexOf('+') !== -1) {
+      counter = newsDOM.substring(0, newsDOM.indexOf('K')) + '000';
+    }
+
+    Franz.setBadge(counter);
+  };
+
+  Franz.loop(getMessages);
+
+}
