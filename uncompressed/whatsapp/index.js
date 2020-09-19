2466d5c99758fe54075b2b6c482998e58932c11a
beppe9000 <6625123+beppe9000@users.noreply.github.com>
2020-09-19 19:00:48 +0200

    Fix UA string for WhatsApp (#275)
    * Fixed UA string for Whatsapp
    
    Works for me. Tested on Windows 10.
    
    * bump version of package
    
    * publish package

--- uncompressed/whatsapp/index.js
+++ uncompressed/whatsapp/index.js
@@ -2,11 +2,14 @@
 
 module.exports = (Franz) =>
   class Messenger extends Franz {
+    overrideUserAgent() {
+      return window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+( \([^)]+\))?/g, "").trim();
+    }
     modifyRequestHeaders() {
       return [
         {
           headers: {
-            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g,"").trim(),
+            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+( \([^)]+\))?/g, "").trim(),
           },
           requestFilters: {
             urls: ['*://*/*'],
