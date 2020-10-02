b2f08a73a3fd71968aefca0f3e5fdee7f545f4d0
Daniel Smith <1657182+StormPooper@users.noreply.github.com>
2020-10-02 14:50:35 +0100

    Update user agent for OWA and Outlook (#302)

--- recipes/office365-owa/index.js
+++ recipes/office365-owa/index.js
@@ -1,1 +1,8 @@
-module.exports = Franz => Franz;
+var os = require('os')
+
+module.exports = Franz =>
+  class Owa extends Franz {
+    overrideUserAgent() {
+      return window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g,"").trim();
+    }
+  };
