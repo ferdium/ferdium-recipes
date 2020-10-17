f4435989a6052852a3f9326d931f63e18ff78758
Ravi Sharma <rvisharma91@gmail.com>
2020-10-18 02:24:11 +0530

    Update Todoist notifications badge selector (#333) (#334)

--- recipes/todoist/webview.js
+++ recipes/todoist/webview.js
@@ -4,8 +4,8 @@ module.exports = (Franz, options) => {
   function getTasks() {
     let todayCount = 0;
     let inboxCount = 0;
-    const todayElement = document.querySelector("#top_filters .counter.overdue_color");
-    const inboxElement = document.querySelector("#filter_inbox .counter");
+    const todayElement = document.querySelector("#filter_today .item_counter");
+    const inboxElement = document.querySelector("#filter_inbox .item_counter");
 
     if (todayElement) {
       todayCount = parseInt(todayElement.innerHTML, 10);
