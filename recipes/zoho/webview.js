4adb208a4805d282ff8ac760dbd978c95e053466
Thomas Papamichail <thomas@pointer.gr>
2020-11-04 14:51:19 +0200

    Fix unread emails css selector

--- recipes/zoho/webview.js
+++ recipes/zoho/webview.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (Franz, options) => {
   const getMessages = () => {
-    const unreadMail = $(".zm_urd").not(".hidden").length;
+    const unreadMail = $(".zmList.zmLUrd").length;
 
     Franz.setBadge(unreadMail);
   }
