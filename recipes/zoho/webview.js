00d132835777ba7ef49f783b0b51edf4c3920e29
Thomas Papamichail <thomas@pointer.gr>
2020-11-05 13:49:20 +0200

    Update version & Show number of unread messages in the current folder & Show badge for unread messages everywhere

--- recipes/zoho/webview.js
+++ recipes/zoho/webview.js
@@ -2,9 +2,10 @@ const path = require('path');
 
 module.exports = (Franz, options) => {
   const getMessages = () => {
-    const unreadMail = $("#zmlTree .zmTreeNDWra .zmBold").length;
+    const unreadMailInCurrentFolder = $(".zmList.zmLUrd").length;
+    const unreadMailAnyware = $("#zmlTree .zmTreeNDWra .zmBold").length;
 
-    Franz.setBadge(unreadMail);
+    Franz.setBadge(unreadMailInCurrentFolder, unreadMailAnyware);
   }
 
   Franz.loop(getMessages);
