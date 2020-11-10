19c290a58c3e6973697d48b3490575cadbbf1b92
Ian Sealy <git@iansealy.com>
2020-11-10 16:47:09 +0000

    Habitica: Add notification count.

--- recipes/habitica/webview.js
+++ recipes/habitica/webview.js
@@ -1,3 +1,17 @@
 "use strict";
 
-module.exports = Franz => Franz;
+module.exports = (Franz) => {
+  const getMessages = () => {
+    let count = 0;
+    let element = document.querySelector(".message-count");
+    if (element) {
+      count = Number(element.innerText);
+      if (Number.isNaN(count)) {
+        count = 0;
+      }
+    }
+    Franz.setBadge(count);
+  };
+
+  Franz.loop(getMessages);
+};
