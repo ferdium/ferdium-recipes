9261c12bd440043b0f305e135fc3362f79c8a834
Cromefire_ <cromefire_@outlook.com>
2021-02-02 21:47:42 +0100

    Remove invalid file (#424)

--- recipes/airtable/webview.js
+++ recipes/airtable/webview.js
@@ -1,17 +1,1 @@
-{
-  "id": "airtable",
-  "name": "Airtable",
-  "version": "1.0.1",
-  "description": "Airtable",
-  "main": "index.js",
-  "author": "Casey Sparks <caseyalexsparks@gmail.com>",
-  "license": "MIT",
-  "repository": "https://github.com/transnat/recipe-airtable",
-  "config": {
-    "serviceURL": "https://airtable.com",
-    "hasTeamId": false,
-    "hasCustomUrl": false,
-    "hasNotificationSound": false,
-    "hasIndirectMessages": false
-  }
-}
+
