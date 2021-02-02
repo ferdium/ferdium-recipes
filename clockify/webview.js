41a4dbc484b6d4c6fe09b2bce7a3364032a01650
Sunny Singh <sunnysinghnitb@gmail.com>
2021-02-03 02:17:03 +0530

    Added recipe for productivity app Clockify (#431)

---
+++ clockify/webview.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = Franz => {
+  const getMessages = function getMessages() {
+    const notifications = document.querySelectorAll('.notification--number');
+
+    Franz.setBadge(0, notifications.length >= 1 ? 1 : 0);
+  };
+
+  Franz.loop(getMessages);
+};
