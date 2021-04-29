fae35abda2953e38106fccd12c7d34226bb54a7c
Vijay Raghavan Aravamudhan <vraravam@users.noreply.github.com>
2021-04-29 19:55:52 +0530

    Fixing issue with image preview not working in gChat. (#476)

--- recipes/hangoutschat/index.js
+++ recipes/hangoutschat/index.js
@@ -3,20 +3,4 @@ var os = require('os')
 // just pass through Franz
 module.exports = Franz =>
   class HangoutsChat extends Franz {
-    // Method to add headers to requests from Hangouts Chat's webview
-    modifyRequestHeaders() {
-      return [{
-        headers: { 'origin': 'https://chat.google.com' },
-        requestFilters: {
-          urls: ['*://*/*']
-        }
-      }]
-    }
-
-    overrideUserAgent() {
-      if (os.platform() == 'linux')
-        return "Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
-      else
-        return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0";
-    }
   };
