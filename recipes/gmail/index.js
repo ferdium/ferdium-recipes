1cbed113537eef0cffe36a72cb2d315adf7348a7
Vijay Raghavan Aravamudhan <vraravam@users.noreply.github.com>
2021-05-11 22:40:43 +0530

    Fixing issue with non-sso login for gmail + gdrive not working due to incompatible user-agent/browser version (#477)

--- recipes/gmail/index.js
+++ recipes/gmail/index.js
@@ -2,16 +2,4 @@ var os = require('os')
 
 module.exports = Franz =>
   class Gmail extends Franz {
-    modifyRequestHeaders() {
-      return [
-        {
-          headers: {
-            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g,"").trim(),
-          },
-          requestFilters: {
-            urls: ['*://*/*'],
-          }
-        }
-      ]
-    }
   };
--- recipes/googledrive/index.js
+++ recipes/googledrive/index.js
@@ -2,17 +2,4 @@ var os = require('os')
 
 module.exports = Franz =>
   class googledrive extends Franz {
-    modifyRequestHeaders() {
-      return [
-        {
-          headers: {
-            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g, '').trim(),
-            'origin': 'https://drive.google.com'
-          },
-          requestFilters: {
-            urls: ['*://*/*'],
-          },
-        },
-      ];
-    }
   };
