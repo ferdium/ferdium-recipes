846157ea2b94c9ef82bcd2bd088118036257ea0d
Vijay A <avijayr@protonmail.com>
2021-05-14 16:45:19 +0530

    Fixing issue with non-sso login for all google services not working due to incompatible user-agent/browser version

--- recipes/googlecalendar/index.js
+++ recipes/googlecalendar/index.js
@@ -1,11 +1,3 @@
-var os = require('os')
-
 module.exports = Franz =>
-  class googlecalendar extends Franz {
-    overrideUserAgent() {
-      if (os.platform() == 'linux')
-        return "Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
-      else
-        return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0";
-    }
+  class GoogleCalendar extends Franz {
   };
--- recipes/googlemeet/index.js
+++ recipes/googlemeet/index.js
@@ -1,17 +1,3 @@
-var os = require('os')
-
 module.exports = Franz =>
   class GoogleMeet extends Franz {
-    modifyRequestHeaders() {
-      return [
-        {
-          headers: {
-            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g, '').trim(),
-          },
-          requestFilters: {
-            urls: ['*://*/*'],
-          },
-        },
-      ];
-    }
   };
