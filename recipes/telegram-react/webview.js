3ade15fdb502aec36d6bb42a5239dc4c886c87c4
Vijay A <avijayr@protonmail.com>
2021-05-27 23:50:23 +0530

    Fixing unread count in telegram-react

--- recipes/telegram-react/webview.js
+++ recipes/telegram-react/webview.js
@@ -1,15 +1,13 @@
 "use strict";
 
-const path = require("path");
-
 module.exports = Franz => {
   const getMessages = function getMessages() {
     let count = 0;
-    const elements = document.querySelectorAll(".dialog-badge:not(.dialog-badge-muted)");
+    const elements = document.querySelectorAll(".chatlist > li:not(.is-muted)");
     if (elements) {
       for (let i = 0; i < elements.length; i += 1) {
-        if (elements[i].querySelector("span").innerHTML !== 0) {
-          count += parseInt(elements[i].querySelector("span").innerHTML);
+        if (elements[i].querySelector(".unread") && elements[i].querySelector(".unread").innerHTML !== 0) {
+          count += parseInt(elements[i].querySelector(".unread").innerHTML);
         }
       }
     }
