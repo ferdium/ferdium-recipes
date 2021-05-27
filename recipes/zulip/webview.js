77b482957237d8a6337e47360bdb8824add56efb
Vijay A <avijayr@protonmail.com>
2021-05-27 21:40:16 +0530

    Re-fetched zulip recipe from https://github.com/adambirds/recipe-zulip

--- recipes/zulip/webview.js
+++ recipes/zulip/webview.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = Franz => {
+module.exports = (Franz) => {
     const getMessages = function getMessages() {
         const allMessages = Math.round(document.querySelectorAll("#global_filters .top_left_all_messages .count .value")[0].innerText);
         Franz.setBadge(allMessages);
