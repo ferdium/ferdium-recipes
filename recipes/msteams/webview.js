14e401db8c52c12c6de08f6394ee2bb61e5a959e
Kristóf Marussy <kristof@marussy.com>
2021-05-27 23:34:40 +0200

    [msteams] Context isolation support

--- recipes/msteams/webview.js
+++ recipes/msteams/webview.js
@@ -2,29 +2,23 @@
 
 const path = require('path');
 
-window.electronSafeIpc = {
-  send: () => null,
-  on: () => null
-};
-window.desktop = undefined;
-
 module.exports = Franz => {
   const getMessages = () => {
     let messages = 0;
     const badge = document.querySelector('.activity-badge.dot-activity-badge .activity-badge');
-
     if (badge) {
       const value = parseInt(badge.innerHTML, 10);
-
       if (!isNaN(value)) {
         messages = value;
       }
     }
-	const indirectMessages = document.querySelectorAll("[class*=channel-anchor][class*=ts-unread-channel]").length;
+
+    const indirectMessages = document.querySelectorAll('[class*=channel-anchor][class*=ts-unread-channel]').length;
 
     Franz.setBadge(messages, indirectMessages);
   };
 
   Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Franz.injectJSUnsafe(path.join(__dirname, 'webview-unsafe.js'));
   Franz.loop(getMessages);
 };
