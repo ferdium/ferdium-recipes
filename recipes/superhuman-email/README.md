5e56414aa64b339dfdd817a8e49e08c5179d3bdd
Vijay A <avijayr@protonmail.com>
2021-06-29 13:27:15 +0530

    Adding new recipe: Superhuman (Fixes #573)

---
+++ recipes/superhuman-email/README.md
@@ -0,0 +1,5 @@
+# Custom Websites for Franz
+This is the official Franz recipe for adding Superhuman to Franz
+
+### How to create your own Franz recipes:
+* [Read the documentation](https://github.com/meetfranz/plugins)
---
+++ recipes/superhuman-email/index.js
@@ -0,0 +1,11 @@
+// just pass through Franz - Superhuman tab appears, says needs Chrome
+// module.exports = Franz => Franz;
+
+// tried the whole string from the issue example - Superhuman tab appears, says needs Chrome
+// module.exports = Franz => class useragent extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
+
+// updated class name to superhuman - Superhuman tab appears, says needs Chrome
+module.exports = Franz => class superhuman extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
+
+// Tried shortening to just Chrome lol - Superhuman tab appears, says needs Chrome
+// module.exports = Franz => class superhuman extends Franz { overrideUserAgent() {return "Chrome"; } };
