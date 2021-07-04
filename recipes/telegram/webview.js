7311682b5f3a1796814757b6605e782bf4de3fde
Vijay A <avijayr@protonmail.com>
2021-07-04 20:39:06 +0530

    Fixing group unread count in telegram recipe.

--- recipes/telegram/webview.js
+++ recipes/telegram/webview.js
@@ -7,16 +7,20 @@ const path = require('path');
 module.exports = Franz => {
   const getMessages = function getMessages() {
     let count = 0;
-    const elements = document.querySelectorAll('.unread');
-    if (elements) {
-      for (let i = 0; i < elements.length; i += 1) {
-        if (parseInt(elements[i].innerText)) {
-          count += parseInt(elements[i].innerText)
+    let count_sec = 0
+    const elements = document.querySelectorAll('.rp');
+    for (let i = 0; i < elements.length; i += 1) {
+      if (elements[i].querySelector('.dialog-subtitle-badge') && (!isNaN(parseInt(elements[i].querySelector('.dialog-subtitle-badge').innerText)) )) {
+        if (parseInt(elements[i].querySelector('.dialog-subtitle-badge').innerText) != "" && (elements[i].dataset.peerId > 0)) {
+          count = +count + parseInt(elements[i].querySelector('.dialog-subtitle-badge').innerText)
+        }
+        else {
+          count_sec = +count_sec + parseInt(elements[i].querySelector('.dialog-subtitle-badge').innerText)
         }
       }
     }
 
-    Franz.setBadge(count);
+    Franz.setBadge(count, count_sec);
   };
 
   Franz.injectCSS(path.join(__dirname, 'service.css'));
