19d587d8ee2d3d1cc6af0284e934f0a904482026
Vijay A <avijayr@protonmail.com>
2021-07-27 20:36:48 +0530

    Allow any url in the custom-service to allow for sites like 'http://translate.google.com' to work.

--- recipes/franz-custom-website/index.js
+++ recipes/franz-custom-website/index.js
@@ -2,16 +2,6 @@
 
 module.exports = Franz => class CustomWebsite extends Franz {
   async validateUrl(url) {
-    try {
-      const resp = await window.fetch(url, {
-        method: 'GET'
-      });
-      return !resp.status.toString().startsWith('4');
-    } catch (err) {
-      console.error(err);
-    }
-
-    return false;
+    return true;
   }
-
 };
