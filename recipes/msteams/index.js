62599c06f4b2039219e282b34a51a4ff4fbe5e67
Vijay A <avijayr@protonmail.com>
2021-09-10 22:36:41 +0530

    Turning off the certificate whitelisting in ms-teams since that can cause an MITM attack.

--- recipes/msteams/index.js
+++ recipes/msteams/index.js
@@ -3,15 +3,16 @@ module.exports = Ferdi => class MicrosoftTeams extends Ferdi {
     return window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g, '').trim();
   }
 
-  // https://docs.microsoft.com/en-us/microsoftteams/troubleshoot/teams-sign-in/sign-in-loop#resolution
-  knownCertificateHosts() {
-    return [
-      'microsoft.com',
-      'microsoftonline.com',
-      'teams.skype.com',
-      'teams.microsoft.com',
-      'sfbassets.com',
-      'skypeforbusiness.com',
-    ];
-  };
+  // TODO: Commenting out since this can expose the user to a MITM attack. Before this can be turned on, we will need to put in some safeguards/warnings that the user can explicitly accept the security ramifications
+  // // https://docs.microsoft.com/en-us/microsoftteams/troubleshoot/teams-sign-in/sign-in-loop#resolution
+  // knownCertificateHosts() {
+  //   return [
+  //     'microsoft.com',
+  //     'microsoftonline.com',
+  //     'teams.skype.com',
+  //     'teams.microsoft.com',
+  //     'sfbassets.com',
+  //     'skypeforbusiness.com',
+  //   ];
+  // };
 };
