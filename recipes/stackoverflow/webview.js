9897921a8f0340d95198a9eea597a7c368557506
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 18:37:25 +0530

    New recipe: 'stackoverflow' (#707)

---
+++ recipes/stackoverflow/webview.js
@@ -0,0 +1,20 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = (Ferdi) => {
+  const getMessages = function getMessages() {
+    const elements = document.querySelectorAll('.CxUIE, .unread');
+    let count = 0;
+    for (let i = 0; i < elements.length; i += 1) {
+      if (elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
+        count += 1;
+      }
+    }
+
+    Ferdi.setBadge(count);
+  };
+
+  Ferdi.loop(getMessages);
+  Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
