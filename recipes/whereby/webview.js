57ba0c384871f14c27428f4b07b233e0fd2f4733
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 07:07:08 +0530

    New service: 'whereby' (#683)

---
+++ recipes/whereby/webview.js
@@ -0,0 +1,25 @@
+var _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = Ferdi => {
+  const getMessages = function getMessages() {
+    const elements = document.querySelectorAll('.CxUIE, .unread');
+    let count = 0;
+
+    for (let i = 0; i < elements.length; i += 1) {
+      if (elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
+        count += 1;
+      }
+    }
+
+    // set Ferdi badge
+    Ferdi.setBadge(count);
+  };
+
+  // check for new messages every second and update Ferdi badge
+  Ferdi.loop(getMessages);
+
+  // inject Ferdi.css stylesheet
+  Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
