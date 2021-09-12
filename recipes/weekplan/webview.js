25ca79240255af73f94832a6a1fe9ba01a95ffcb
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 18:16:44 +0530

    New recipe: 'weekplan' (#705)

---
+++ recipes/weekplan/webview.js
@@ -0,0 +1,10 @@
+module.exports = Ferdi => {
+  getMessages = () => {
+    const events = document.querySelectorAll(".today")[0].getElementsByClassName('list')[0].getElementsByClassName('task-list')[0].getElementsByClassName('event-section')[0].getElementsByClassName('droppable')[0].children[0].childElementCount
+    const tasks = document.querySelectorAll(".today")[0].getElementsByClassName('list')[0].getElementsByClassName('task-list')[0].getElementsByClassName('task-section')[0].getElementsByClassName('droppable')[0].children[0].childElementCount
+
+    Ferdi.setBadge(events + tasks);
+  };
+
+  Ferdi.loop(getMessages);
+}
