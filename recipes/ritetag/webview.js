56d21de5b823d3783b783ab6bb26b90cda705ee3
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 17:57:26 +0530

    New recipe: 'ritetag' (#702)

---
+++ recipes/ritetag/webview.js
@@ -0,0 +1,7 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = (Ferdi) => {
+  Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
