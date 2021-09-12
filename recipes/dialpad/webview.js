6d520ff1ddb74e7257e1d943f8e91117079db675
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 13:24:51 +0530

    New recipe: 'dialpad' (#695)

---
+++ recipes/dialpad/webview.js
@@ -0,0 +1,16 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = (Ferdi) => {
+  function getMessages() {
+    var unreadCount = 0;
+    $.each($('[data-qa-has-unreads]'), (idx, item) => unreadCount += Ferdi.safeParseInt(item.attributes["data-qa-has-unreads"].value));
+
+    Ferdi.setBadge(unreadCount);
+  }
+
+  Ferdi.loop(getMessages);
+
+  Ferdi.injectCSS(_path.default.join(__dirname, 'whitemode.css'));
+};
