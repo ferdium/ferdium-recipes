97618c92c1d8f07e032974e1b0d5da580c4c483e
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 17:31:57 +0530

    New recipe: 'freshdesk' (#700)

---
+++ recipes/freshdesk/index.js
@@ -0,0 +1,2 @@
+module.exports = Ferdi => class Freshdesk extends Ferdi {
+};
---
+++ recipes/freshdesk/webview.js
@@ -0,0 +1,14 @@
+module.exports = Ferdi => {
+  const getMessages = function getMessages() {
+    $.get('/api/_/tickets?filter=unresolved', (data) => {
+      Ferdi.setBadge(data.tickets.length);
+    });
+  };
+
+  Ferdi.loop(getMessages);
+
+/* block popups (prevents freshconnect from opening in a new window) */
+  window.open = (function(url, name) {
+    console.log(`blocked window.open(${url}, ${name})`);
+  });
+};
