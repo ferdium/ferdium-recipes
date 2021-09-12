93738624ed1dd4473fb4e6cdfb859e6250437104
Vijay A <avijayr@protonmail.com>
2021-09-12 15:58:12 +0530

    Merged 'github_enterprise' recipe into 'github' with the extra bits.

--- recipes/github/index.js
+++ recipes/github/index.js
@@ -1,1 +1,19 @@
-module.exports = Ferdi => Ferdi;
+module.exports = Ferdi => class GitHub extends Ferdi {
+  async validateUrl(url) {
+    try {
+      const resp = await window.fetch(`${url}/api/v3`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/vnd.github.v3+json',
+        },
+      });
+      // doc: https://developer.github.com/enterprise/2.17/v3/#current-version
+      return resp.status.toString().startsWith('2');
+    } catch (err) {
+      console.error(err);
+    }
+
+    return false;
+  }
+};
