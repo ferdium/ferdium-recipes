054fb8b5e82bf4387ecafb6df1dca5222582e6a6
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 18:12:00 +0530

    New recipe: 'sococo' (#704)

---
+++ recipes/sococo/webview.js
@@ -0,0 +1,12 @@
+module.exports = (Ferdi) => {
+  const getMessages = function getMessages() {
+    let indirect = document.querySelectorAll('.new-messages');
+    let direct = 0;
+    document.querySelectorAll('.people-pane .badge').forEach(function(badge){
+      direct += Ferdi.safeParseInt(badge.innerText);
+    });
+    Ferdi.setBadge(direct, indirect);
+  };
+
+  Ferdi.loop(getMessages);
+};
