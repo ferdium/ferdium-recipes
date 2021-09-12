b3411bfca0dd166995d33760feb4ab2c89537c46
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-09-12 18:06:39 +0530

    Add indirect count for 'teamleader' recipe. (#703)

--- recipes/teamleader/webview.js
+++ recipes/teamleader/webview.js
@@ -3,11 +3,29 @@ const _path = _interopRequireDefault(require('path'));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = (Ferdi) => {
-  const getMessages = () => {
-    Ferdi.setBadge(document.getElementById('notifications_amount').innerHTML);
+  const getMessages = function getMessages() {
+    let notifications = 0;
+    let indirectNotifications = 0;
+
+    const notification_element = document.getElementById('notifications_amount');
+    const ticket_element = document.querySelector("a[href='tickets.php'] > span");
+    const call_element = document.getElementById('queue_amount');
+
+    if (notification_element) {
+        notifications = Ferdi.safeParseInt(notification_element.getAttribute("datacount"));
+    }
+
+    if (ticket_element != null) {
+      indirectNotifications = Ferdi.safeParseInt(ticket_element.innerHTML);
+    }
+
+    if (call_element) {
+      indirectNotifications += Ferdi.safeParseInt(call_element.getAttribute("datacount"));
+    }
+
+    Ferdi.setBadge(notifications, indirectNotifications);
   };
 
   Ferdi.loop(getMessages);
-
-  Ferdi.injectCSS(_path.default.join(__dirname, 'css', 'franz.css'));
+  Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
