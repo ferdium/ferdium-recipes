3a4e7792778b092038b3efb2d39ad656ce791599
Vijay A <avijayr@protonmail.com>
2021-09-23 08:00:46 +0530

    fix: updated 'skype' and 'discord' to properly relinquish 'win' object for image overlay

--- recipes/discord/webview.js
+++ recipes/discord/webview.js
@@ -29,6 +29,7 @@ module.exports = (Ferdi, settings) => {
 
   Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
 
+  // TODO: This whole block is duplicated between the 'discord' and 'skype' recipes - reuse
   document.addEventListener('click', event => {
     const link = event.target.closest('a[href^="http"]');
     const button = event.target.closest('button[title^="http"]');
@@ -39,12 +40,17 @@ module.exports = (Ferdi, settings) => {
       if (url.includes('views/imgpsh_fullsize_anim')) {
         event.preventDefault();
         event.stopPropagation();
+        // TODO: Can we send an ipc event 'open-browser-window' to open the child window? (see the slack recipe for how to send an ipc message)
+        // TODO: Can we change the slack recipe to add a clickHandler for screensharing/video calls? (https://github.com/getferdi/ferdi/issues/1697)
         let win = new Ferdi.BrowserWindow({
           width: 800,
           height: window.innerHeight,
           minWidth: 600,
           webPreferences: {
             partition: `persist:service-${settings.id}`
+            // TODO: Aren't these needed here?
+            // contextIsolation: false,
+            // enableRemoteModule: true,
           }
         });
         win.loadURL(url);
