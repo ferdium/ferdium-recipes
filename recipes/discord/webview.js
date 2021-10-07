a1a7a4e5d2868dcec98db86c2692e65800a4fb57
Markus Hatvan <markus_hatvan@aon.at>
2021-10-07 11:01:41 +0200

    fix: discord does not show notifications (#734)

--- recipes/discord/webview.js
+++ recipes/discord/webview.js
@@ -1,28 +1,25 @@
 const _path = _interopRequireDefault(require('path'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 module.exports = (Ferdi, settings) => {
   const getMessages = () => {
-    let count = 0;
-    const container = document.querySelector('[role="tablist"] > [title="Chats"] > div');
+    let directCount = 0;
+    const directCountPerServer = document.querySelectorAll(
+      '[class*="lowerBadge-"] [class*="numberBadge-"]',
+    );
 
-    if (container) {
-      const children = container.children;
-
-      if (children.length === 3) {
-        const elementContainer = children[children.length - 1];
-
-        if (elementContainer) {
-          const element = elementContainer.querySelector('[data-text-as-pseudo-element]');
-          if (element && element.dataset) {
-            count = Ferdi.safeParseInt(element.dataset.textAsPseudoElement);
-          }
-        }
-      }
+    for (const directCountBadge of directCountPerServer) {
+      directCount += Ferdi.safeParseInt(directCountBadge.textContent);
     }
 
-    Ferdi.setBadge(count);
+    const indirectCountPerServer = document.querySelectorAll(
+      '[class*="modeUnread-"]',
+    ).length;
+
+    Ferdi.setBadge(directCount, indirectCountPerServer);
   };
 
   Ferdi.loop(getMessages);
@@ -30,34 +27,40 @@ module.exports = (Ferdi, settings) => {
   Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
 
   // TODO: This whole block is duplicated between the 'discord' and 'skype' recipes - reuse
-  document.addEventListener('click', event => {
-    const link = event.target.closest('a[href^="http"]');
-    const button = event.target.closest('button[title^="http"]');
+  document.addEventListener(
+    'click',
+    event => {
+      const link = event.target.closest('a[href^="http"]');
+      const button = event.target.closest('button[title^="http"]');
 
-    if (link || button) {
-      const url = link ? link.getAttribute('href') : button.getAttribute('title');
+      if (link || button) {
+        const url = link
+          ? link.getAttribute('href')
+          : button.getAttribute('title');
 
-      if (url.includes('views/imgpsh_fullsize_anim')) {
-        event.preventDefault();
-        event.stopPropagation();
-        // TODO: Can we send an ipc event 'open-browser-window' to open the child window? (see the slack recipe for how to send an ipc message)
-        // TODO: Can we change the slack recipe to add a clickHandler for screensharing/video calls? (https://github.com/getferdi/ferdi/issues/1697)
-        let win = new Ferdi.BrowserWindow({
-          width: 800,
-          height: window.innerHeight,
-          minWidth: 600,
-          webPreferences: {
-            partition: `persist:service-${settings.id}`
-            // TODO: Aren't these needed here?
-            // contextIsolation: false,
-            // enableRemoteModule: true,
-          }
-        });
-        win.loadURL(url);
-        win.on('closed', () => {
-          win = null;
-        });
+        if (url.includes('views/imgpsh_fullsize_anim')) {
+          event.preventDefault();
+          event.stopPropagation();
+          // TODO: Can we send an ipc event 'open-browser-window' to open the child window? (see the slack recipe for how to send an ipc message)
+          // TODO: Can we change the slack recipe to add a clickHandler for screensharing/video calls? (https://github.com/getferdi/ferdi/issues/1697)
+          let win = new Ferdi.BrowserWindow({
+            width: 800,
+            height: window.innerHeight,
+            minWidth: 600,
+            webPreferences: {
+              partition: `persist:service-${settings.id}`,
+              // TODO: Aren't these needed here?
+              // contextIsolation: false,
+              // enableRemoteModule: true,
+            },
+          });
+          win.loadURL(url);
+          win.on('closed', () => {
+            win = null;
+          });
+        }
       }
-    }
-  }, true);
+    },
+    true,
+  );
 };
