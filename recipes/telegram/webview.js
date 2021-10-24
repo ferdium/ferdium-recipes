4a6fcf1a35f6d26f7c0d04c9769ca101df8f6ce8
Markus Hatvan <markus_hatvan@aon.at>
2021-10-24 10:38:34 +0200

    feat: support telegram count for both webk and webz (#751)

--- recipes/telegram/webview.js
+++ recipes/telegram/webview.js
@@ -1,5 +1,3 @@
-// Code copied from: https://gitlab.com/gortega4/ferdi_recipes
-
 const _path = _interopRequireDefault(require('path'));
 
 function _interopRequireDefault(obj) {
@@ -7,27 +5,76 @@ function _interopRequireDefault(obj) {
 }
 
 module.exports = Ferdi => {
-  const getMessages = () => {
-    let direct = 0;
-    let indirect = 0;
-    const elements = document.querySelectorAll('.rp');
+  const telegramVersion = document
+    .querySelector('meta[name="application-name"]')
+    ?.getAttribute('content');
+
+  const isWebZ = telegramVersion?.includes('WebZ');
+  const isWebK = telegramVersion?.includes('WebK');
+
+  // There are two different Telegram versions for internal competition
+  // Read more: https://bugs.telegram.org/c/4002/public
+  const webZCount = () => {
+    let directCount = 0;
+    let groupCount = 0;
+
+    const directCountSelector = document.querySelectorAll(
+      '.chat-list .ListItem.private .Badge.unread:not(.muted)',
+    );
+    const groupCountSelector = document.querySelectorAll(
+      '.chat-list .ListItem.group .Badge.unread:not(.muted)',
+    );
+
+    for (const badge of directCountSelector) {
+      directCount += Ferdi.safeParseInt(badge.textContent);
+    }
+
+    for (const badge of groupCountSelector) {
+      groupCount += Ferdi.safeParseInt(badge.textContent);
+    }
+
+    Ferdi.setBadge(directCount, groupCount);
+  };
+
+  const webKCount = () => {
+    let directCount = 0;
+    let groupCount = 0;
+
+    const elements = document.querySelectorAll('.rp:not(.is-muted)');
+
     for (const element of elements) {
       const subtitleBadge = element.querySelector('.dialog-subtitle-badge');
+
       if (subtitleBadge) {
         const parsedValue = Ferdi.safeParseInt(subtitleBadge.textContent);
+
         if (element.dataset.peerId > 0) {
-          direct += parsedValue;
+          directCount += parsedValue;
         } else {
-          indirect += parsedValue;
+          groupCount += parsedValue;
         }
       }
     }
 
-    Ferdi.setBadge(direct, indirect);
+    Ferdi.setBadge(directCount, groupCount);
+  };
+
+  const getMessages = () => {
+    if (isWebZ) {
+      webZCount();
+    } else if (isWebK) {
+      webKCount();
+    }
   };
 
   const getActiveDialogTitle = () => {
-    const element = document.querySelector('.top .peer-title');
+    let element;
+
+    if (isWebZ) {
+      element = document.querySelector('.chat-list .ListItem .title > h3');
+    } else if (isWebK) {
+      element = document.querySelector('.top .peer-title');
+    }
 
     Ferdi.setDialogTitle(element ? element.textContent : '');
   };
