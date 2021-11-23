b88f3a009471711299a5878fdadf6d197b505f27
Vijay A <vraravam@users.noreply.github.com>
2021-11-23 21:23:48 +0530

    Fixing issue with 'toggl' recipe exposed by the pre-commit hook

--- recipes/toggl/webview-unsafe.js
+++ recipes/toggl/webview-unsafe.js
@@ -1,11 +1,7 @@
 // only try to update badge once Ferdi API has finished loading
-if (ferdi != undefined && ferdi.setBadge != undefined) {
-    var timerRunning = (window.toggl != undefined) && !!(window.toggl.store.getState().view.timer.timeEntry.start);
+if (window.ferdi != undefined && window.ferdi.setBadge != undefined) {
+  var timerRunning = (window.toggl != undefined) && !!(window.toggl.store.getState().view.timer.timeEntry.start);
 
-    if (timerRunning) {
-        // Treat running timer as a "non-direct" notification (default blue dot instead of urgent red "1")
-        ferdi.setBadge(0, 1);
-    } else {
-        ferdi.setBadge(0);
-    }
+  // Treat running timer as a "non-direct" notification (default blue dot instead of urgent red "1")
+  window.ferdi.setBadge(0, timerRunning ? 1 : 0);
 }
