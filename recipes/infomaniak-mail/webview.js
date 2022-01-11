f82f019aa993f346c8997d88944c055a377e3f37
Bauke van der Woude <6784391+baukevdw@users.noreply.github.com>
2022-01-11 23:57:30 +0100

    Fix Infomaniak unread messages count (#817)

--- recipes/infomaniak-mail/webview.js
+++ recipes/infomaniak-mail/webview.js
@@ -1,15 +1,8 @@
 module.exports = Ferdi => {
   const getMessages = () => {
     const count = document.querySelector('.ws-tree-node-badge');
-    if (count) {
-      const countText = count.textContent;
-      if (countText) {
-        Ferdi.setBadge(
-          // eslint-disable-next-line unicorn/prefer-string-slice
-          count ? Number(countText.substring(1, countText.length - 1)) : 0,
-        );
-      }
-    }
+    const countText = count ? count.textContent : null;
+    Ferdi.setBadge(count && countText ? Number(countText.substring(1, countText.length - 1)) : 0);
   };
 
   Ferdi.loop(getMessages);
