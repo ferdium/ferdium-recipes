e32c7afca34149d856634e4a3a75892c5606c191
Bauke van der Woude <6784391+baukevdw@users.noreply.github.com>
2022-01-20 17:21:09 +0100

    Fix Infomaniak message count (#823)

--- recipes/infomaniak-mail/webview.js
+++ recipes/infomaniak-mail/webview.js
@@ -2,7 +2,7 @@ module.exports = Ferdi => {
   const getMessages = () => {
     const count = document.querySelector('.ws-tree-node-badge');
     const countText = count ? count.textContent : null;
-    Ferdi.setBadge(count && countText ? Number(countText.substring(1, countText.length - 1)) : 0);
+    Ferdi.setBadge(count && countText ? Number(countText) : 0);
   };
 
   Ferdi.loop(getMessages);
