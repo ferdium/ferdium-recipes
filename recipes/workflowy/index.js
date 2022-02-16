ed79419783ebf85056a27cdcd8802eac0d802f81
tukiplus <98679174+tukiplus@users.noreply.github.com>
2022-02-16 20:33:50 +0900

    Add WorkFlowy recipe (#829)

---
+++ recipes/workflowy/index.js
@@ -0,0 +1,1 @@
+module.exports = Ferdi => class Workflowy extends Ferdi {};
---
+++ recipes/workflowy/webview.js
@@ -0,0 +1,8 @@
+module.exports = () => {
+  // TODO: If your workflowy service has unread messages, uncomment these lines to implement the logic for updating the badges
+  // const getMessages = () => {
+  //   // TODO: Insert your notification-finding code here
+  //   Ferdi.setBadge(0, 0);
+  // };
+  // Ferdi.loop(getMessages);
+};
