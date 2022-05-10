40f50793a4f64b22190dfc3d3993b0ecd6d24962
mydarkstar <git@mydarkstar.net>
2022-05-10 21:01:28 +0200

    Improve logic and readability

--- recipes/messenger/webview.js
+++ recipes/messenger/webview.js
@@ -2,11 +2,13 @@ module.exports = Ferdium => {
   const getMessages = () => {
     let count = Array.from(document.querySelectorAll('.bp9cbjyn.j83agx80.owycx6da:not(.btwxx1t3)'))
       .map(elem => {
-        const hasPing = Boolean(elem.querySelector('.pq6dq46d.is6700om.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.s45kfl79.emlxlaya.bkmhp75w.spb7xbtv.cyypbtt7.fwizqjfa'))
-        const isMuted = Boolean(elem.querySelector('.a8c37x1j.ms05siws.l3qrxjdp.b7h9ocf4.trssfv1o'))
-        return hasPing - isMuted >= 0 ? hasPing - isMuted : 0;
+        const hasPing = !!elem.querySelector('.pq6dq46d.is6700om.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.s45kfl79.emlxlaya.bkmhp75w.spb7xbtv.cyypbtt7.fwizqjfa');
+        const isMuted = !!elem.querySelector('.a8c37x1j.ms05siws.l3qrxjdp.b7h9ocf4.trssfv1o');
+
+        return hasPing && !isMuted;
       })
       .reduce((prev, curr) => prev + curr, 0);
+
     /*
      * add count of message requests on top of notification counter
      */
