557dfc26b3e446d7a0329565786c8cedfe3d3b55
Vijay A <vraravam@users.noreply.github.com>
2022-05-15 02:18:13 -0500

    Fix unread message count for zulip

--- recipes/zulip/webview.js
+++ recipes/zulip/webview.js
@@ -1,12 +1,24 @@
 module.exports = Ferdium => {
   const getMessages = () => {
-    const allMessages = Math.round(
-      document.querySelectorAll(
-        '#global_filters .top_left_all_messages .count .value',
-      )[0].textContent,
-    );
-    Ferdium.setBadge(allMessages);
+    // All unread messages
+    const unreadAll = getUnreadCount('top_left_all_messages');
+
+    // Private messages
+    const unreadPrivate = getUnreadCount('top_left_private_messages');
+
+    // @ Mentions messages
+    const unreadMentions = getUnreadCount('top_left_mentions');
+
+    const directMessages = unreadPrivate + unreadMentions;
+    const indirectMessages = unreadAll - directMessages;
+
+    Ferdium.setBadge(directMessages, indirectMessages);
   };
 
   Ferdium.loop(getMessages);
+
+  function getUnreadCount(eltClassName) {
+    const elt = document.querySelectorAll(`#global_filters .${eltClassName} .unread_count`)[0];
+    return elt == null ? 0 : Ferdium.safeParseInt(elt.textContent);
+  }
 };
