28c2f796d39573fe92b27d76ee18907ab8b1ec70
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-06-06 12:34:12 +0100

    Fix #41

---
+++ recipes/epicgames/webview.js
@@ -0,0 +1,35 @@
+module.exports = Ferdium => {
+    const getMessages = () => {
+      let direct = 0;
+      let indirect = 0;
+      const elements = document.querySelectorAll('.rp');
+      for (const element of elements) {
+        const subtitleBadge = element.querySelector('.dialog-subtitle-badge');
+        if (subtitleBadge) {
+          const parsedValue = Ferdium.safeParseInt(subtitleBadge.textContent);
+          if (element.dataset.peerId > 0) {
+            direct += parsedValue;
+          } else {
+            indirect += parsedValue;
+          }
+        }
+      }
+  
+      Ferdium.setBadge(direct, indirect);
+    };
+  
+    const getActiveDialogTitle = () => {
+      const element = document.querySelector('.top .peer-title');
+  
+      Ferdium.setDialogTitle(element ? element.textContent : '');
+    };
+  
+    const loopFunc = () => {
+      getMessages();
+      getActiveDialogTitle();
+    };
+  
+    Ferdium.loop(loopFunc);
+  
+    Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
