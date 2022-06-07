5402c55af969aeb30bfff751a963404832e16aaa
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-06-07 11:56:50 +0100

    Add __path definition

--- recipes/anonaddy/webview.js
+++ recipes/anonaddy/webview.js
@@ -1,3 +1,9 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
 module.exports = Ferdium => {
     const getMessages = () => {
       let direct = 0;
