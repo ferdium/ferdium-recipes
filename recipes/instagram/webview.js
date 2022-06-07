669556779265421d4308ade3947e7953e704672b
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-06-07 11:49:26 +0100

    Fix after review

--- recipes/instagram/webview.js
+++ recipes/instagram/webview.js
@@ -15,13 +15,34 @@ module.exports = Ferdium => {
   // https://github.com/ferdium/ferdium-recipes/blob/9d715597a600710c20f75412d3dcd8cdb7b3c39e/docs/frontend_api.md#usage-4
   // Helper that activates DarkReader and injects your darkmode.css at the same time
   Ferdium.handleDarkMode((isEnabled) => {
-    const baseURL = window.location.href;
-    const nativeDarkTheme = '?theme=dark';
-    if (!baseURL.includes(nativeDarkTheme) && isEnabled) {
-      window.location.href = [...baseURL, nativeDarkTheme];
-    } else if (baseURL.includes(nativeDarkTheme) && !isEnabled) {
-      window.location.href = baseURL.replace(nativeDarkTheme,'');
+
+    var url = new URL(window.location.href);
+    var search_params = url.searchParams;
+    var isDarkModeParam = search_params.get('theme');
+    var changedParams = false;
+
+    if (isEnabled) {
+      isDarkModeParam ? null :
+        (
+          search_params.set('theme', 'dark'),
+          changedParams = true
+        );
+    } else {
+      isDarkModeParam ? 
+        (
+          search_params.delete('theme', 'dark'),
+          changedParams = true
+        )
+      : null;
     }
+
+    changedParams ? 
+      (
+        url.search = search_params.toString(),
+        window.location.href = url.toString()
+      )
+    : null;
+
   });
 
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
