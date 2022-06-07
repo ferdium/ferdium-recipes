88b09033d1ecd84ef77f7ba1297fbfd6f884fc96
Bernhard <bernhard@kralofsky.com>
2022-06-07 13:58:02 +0200

    [Gmail] Add support for priority inbox

--- recipes/gmail/webview.js
+++ recipes/gmail/webview.js
@@ -15,8 +15,8 @@ module.exports = Ferdium => {
   }
 
   const getMessages = () => {
-    let count = 0;
-
+    let countImportant = 0;
+    let countNonImportant = 0;
     const inboxLinks = document.querySelectorAll('.J-Ke.n0');
     if (inboxLinks.length > 0) {
       let parentNode = inboxLinks[0].parentNode;
@@ -25,14 +25,23 @@ module.exports = Ferdium => {
         if (parentNodeOfParentNode) {
           const unreadCounts = parentNodeOfParentNode.querySelectorAll('.bsU');
           if (unreadCounts.length > 0) {
-            count = Ferdium.safeParseInt(unreadCounts[0].textContent.replace(/[^\p{N}]/gu, ''));
+            let unreadCount = unreadCounts[0].textContent;
+            if (unreadCount.includes(':')) {
+              let counts = unreadCount
+                .split(':')
+                .map(s => Ferdium.safeParseInt(s.replace(/[^\p{N}]/gu, '')));
+              countImportant = counts[0];
+              countNonImportant = counts[1] - counts[0];
+            } else {
+              countImportant = Ferdium.safeParseInt(
+                unreadCount.replace(/[^\p{N}]/gu, ''),
+              );
+            }
           }
         }
       }
     }
-
-    // set Ferdium badge
-    Ferdium.setBadge(count);
+    Ferdium.setBadge(countImportant, countNonImportant);
   };
 
   Ferdium.loop(getMessages);
