26f2e9ceb1d31d47928350e5cdbde1d52f178344
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-06-07 13:14:42 +0100

    Ferdi to Ferdium and add webview.js

--- recipes/tchap/index.js
+++ recipes/tchap/index.js
@@ -1,8 +1,1 @@
-module.exports = Ferdi => {
-    const getMessages = () => {
-      let count = document.querySelector('.mx_RoomSubList_badge').innerHTML
-      Ferdi.setBadge(count);
-    };
-
-    Ferdi.loop(getMessages);
-  };
+module.exports = Ferdium => Ferdium;
