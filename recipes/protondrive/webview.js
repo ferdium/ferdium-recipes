e118745fc2714047d0844061ed963c47edef3bc1
hayk <haykh.mfs+github@gmail.com>
2022-06-22 21:40:14 -0400

    New recipe 'protondrive' added (#61)
    Co-authored-by: Vijay A <vraravam@users.noreply.github.com>

---
+++ recipes/protondrive/webview.js
@@ -0,0 +1,8 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = (Ferdium) => {
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+  Ferdium.injectJSUnsafe(_path.default.join(__dirname, 'webview-unsafe.js'));
+};
