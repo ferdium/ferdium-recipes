7eef5c339f73b02494ecea74c5ea084c842b1685
Kishan B <kishancs46@gmail.com>
2022-07-03 18:21:44 +0530

    Add user agent fix for ARM macs (Fix #31)

--- recipes/discord/index.js
+++ recipes/discord/index.js
@@ -1,8 +1,8 @@
 module.exports = Ferdium => class Discord extends Ferdium {
   overrideUserAgent() {
-    const useragent = window.navigator.userAgent;
-    const parts = useragent.split('(KHTML, like Gecko)');
-
-    return parts.join('(KHTML, like Gecko) discord/0.0.250').replace('Electron', 'Discord').replace('Ferdium', 'Discord');
+    return window.navigator.userAgent.replace('(KHTML, like Gecko)', '(KHTML, like Gecko) discord/0.0.250')
+      .replace('Electron', 'Discord')
+      .replace('Ferdium', 'Discord')
+      .replace('Apple Mac OS X', 'Intel Mac OS X');
   }
 };
