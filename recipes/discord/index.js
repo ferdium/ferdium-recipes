c3997540e7348146d7796471d6353cb1858431a9
Kishan B <kishancs46@gmail.com>
2022-07-03 22:38:45 +0530

    Replace hardcoding with a specific override for mac os x

--- recipes/discord/index.js
+++ recipes/discord/index.js
@@ -1,5 +1,5 @@
 module.exports = Ferdium => class Discord extends Ferdium {
   overrideUserAgent() {
-    return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
+    return window.navigator.userAgent.replace("Apple Mac OS X", "Intel Mac OS X");
   }
 };
