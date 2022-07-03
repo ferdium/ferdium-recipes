e1fd2aa0873e4d4b8833a69fd679afab4e836394
Vijay A <vraravam@users.noreply.github.com>
2022-07-04 05:02:31 +0530

    Resurrect older userAgent manipulations for discord service

--- recipes/discord/index.js
+++ recipes/discord/index.js
@@ -1,5 +1,8 @@
 module.exports = Ferdium => class Discord extends Ferdium {
   overrideUserAgent() {
-    return window.navigator.userAgent.replace("Apple Mac OS X", "Intel Mac OS X");
+    return window.navigator.userAgent.replace('(KHTML, like Gecko)', '(KHTML, like Gecko) discord/0.0.250')
+      .replace('Electron', 'Discord')
+      .replace('Ferdium', 'Discord')
+      .replace('Apple Mac OS X', 'Intel Mac OS X');
   }
 };
