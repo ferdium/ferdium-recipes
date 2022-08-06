7e4b67b0af03ccfc22a6fef60a79b83956511369
David Kennedy Souza Araújo <daviduser.guj@gmail.com>
2022-08-06 09:50:24 -0300

    Add Grammarly recipe (#140)
    Co-authored-by: André Oliveira <37463445+SpecialAro@users.noreply.github.com>

---
+++ recipes/grammarly/webview.js
@@ -0,0 +1,9 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+module.exports = Ferdium => {
+  Ferdium.injectCSS(_path.default.join(__dirname, 'style.css'));
+};
