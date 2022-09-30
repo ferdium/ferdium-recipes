ed1a481a781ec19f047f47897bd278fa64787dfb
Martin Nygaard Jensen <martin@ravager.dk>
2022-09-30 14:18:54 +0200

    Added Citrix Workspace recipe

---
+++ recipes/citrix-workspace/webview.js
@@ -0,0 +1,8 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+module.exports = Ferdium => {
+};
