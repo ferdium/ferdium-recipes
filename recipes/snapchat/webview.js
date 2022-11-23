e7488bc4083503ef637474f24460bde777ab2c48
Brian Choromanski <BrianChoromanski@gmail.com>
2022-11-23 17:20:12 -0500

    Added getMessages function to Snapchat

--- recipes/snapchat/webview.js
+++ recipes/snapchat/webview.js
@@ -5,12 +5,14 @@ function _interopRequireDefault(obj) {
 }
 
 module.exports = Ferdium => {
-  // TODO: If your SNAME service has unread messages, uncomment these lines to implement the logic for updating the badges
-  // const getMessages = () => {
-  //   // TODO: Insert your notification-finding code here
-  //   Ferdium.setBadge(0, 0);
-  // };
-  // Ferdium.loop(getMessages);
+  const getMessages = () => {
+    const title = document.querySelector('title').textContent.match(/\d+/);
+    const count = title !== null ? title[0] : 0;
+
+    Ferdium.setBadge(count);
+  };
+
+  Ferdium.loop(getMessages);
 
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
