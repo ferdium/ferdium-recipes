ff551ee3375f35335f30c8bc8c6c5b7645d12cfe
Edgars <Edgars+GitHub@gaitenis.id.lv>
2022-11-30 10:13:27 +0200

    Fix notifications count for Nextcloud 25

--- recipes/nextcloud/webview.js
+++ recipes/nextcloud/webview.js
@@ -1,7 +1,9 @@
 module.exports = Ferdium => {
   const getMessages = () => {
-    const direct = document.querySelectorAll(
-      '.notifications .notification-container .notification-wrapper li .notification',
+    const direct = document.querySelector(
+      '#header-menu-notifications, .notifications'
+    ).querySelectorAll(
+      '.notification-container .notification-wrapper .notification'
     ).length;
 
     Ferdium.setBadge(direct);
