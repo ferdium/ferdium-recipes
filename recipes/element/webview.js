3a179ab6fb0859da7acf81286bfde035495e6089
Vijay A <vraravam@users.noreply.github.com>
2022-12-08 07:36:10 +0530

    Auto-fix from eslint triggered a version bump in element recipe

--- recipes/element/webview.js
+++ recipes/element/webview.js
@@ -2,9 +2,9 @@ module.exports = Ferdium => {
   function getMessages() {
     let directCount = 0;
     const spacesBar = document.querySelector('.mx_SpaceTreeLevel');
-    spacesBar.querySelectorAll('.mx_NotificationBadge_count').forEach((badge) => {
+    for (const badge of spacesBar.querySelectorAll('.mx_NotificationBadge_count')) {
       directCount += Ferdium.safeParseInt(badge.textContent);
-    });
+    }
     const indirectCount = spacesBar.querySelectorAll('.mx_NotificationBadge_dot')
       .length;
     Ferdium.setBadge(directCount, indirectCount);
