0f136f7b235539297cf3e7296bcf25246a82fae3
Edgars <Edgars+GitHub@gaitenis.id.lv>
2022-12-28 09:58:46 +0200

    Use unread rooms count like Element app does
    This change uses unread rooms instead of messages count like Element
    (web)app does
    Unread rooms count is extracted from the `title` tag.

--- recipes/element/webview.js
+++ recipes/element/webview.js
@@ -1,11 +1,10 @@
 module.exports = Ferdium => {
   function getMessages() {
-    let directCount = 0;
-    const spacesBar = document.querySelector('.mx_SpaceTreeLevel');
-    for (const badge of spacesBar.querySelectorAll('.mx_NotificationBadge_count')) {
-      directCount += Ferdium.safeParseInt(badge.textContent);
-    }
-    const indirectCount = spacesBar.querySelectorAll('.mx_NotificationBadge_dot')
+    const matches = document.querySelector('title').textContent
+      .match('(?<=\\[)\\d+(?=])');
+    const directCount = Ferdium.safeParseInt(matches !== null ? matches[0] : 0);
+    const indirectCount = document.querySelector('.mx_SpaceTreeLevel')
+      .querySelectorAll('.mx_NotificationBadge_dot')
       .length;
     Ferdium.setBadge(directCount, indirectCount);
   }
