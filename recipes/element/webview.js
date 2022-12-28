84a7c99ec8790495daa85873bcb19f480ae5c56e
Edgars <Edgars+GitHub@gaitenis.id.lv>
2022-12-28 10:04:54 +0200

    Fix unread notifications count for Element
    The following fixes were made:
    
    - take only the space "Home" and ignore other built-in spaces (if they
      are enabled) as count in "Home" badge includes notifications from
      other built-in spaces as well;
    - calculate approximate count from composite numbers (1.2K, 2.3K).

--- recipes/element/webview.js
+++ recipes/element/webview.js
@@ -2,9 +2,20 @@ module.exports = Ferdium => {
   function getMessages() {
     let directCount = 0;
     const spacesBar = document.querySelector('.mx_SpaceTreeLevel');
-    for (const badge of spacesBar.querySelectorAll('.mx_NotificationBadge_count')) {
-      directCount += Ferdium.safeParseInt(badge.textContent);
+
+    for (const badge of spacesBar.querySelectorAll(
+      '.mx_SpaceItem:not(.mx_SpaceItem_narrow):first-of-type .mx_NotificationBadge_count, '
+        + '.mx_SpaceItem_narrow .mx_NotificationBadge_count'
+    )) {
+      const badgeContent = badge.textContent.toLowerCase();
+      directCount += badgeContent.endsWith('k')
+        ? Number.parseFloat(badgeContent) * 1000 + Number.parseInt(
+          '5'.padEnd(Number.parseInt(badgeContent, 10).toString().length + 1, 0),
+          10
+        )
+        : Ferdium.safeParseInt(badgeContent)
     }
+
     const indirectCount = spacesBar.querySelectorAll('.mx_NotificationBadge_dot')
       .length;
     Ferdium.setBadge(directCount, indirectCount);
