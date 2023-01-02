7f4755ad4938fe7bc0fdb003e0d9f6f455702914
Raphael Jenni <info@rjenni.ch>
2023-01-02 11:51:08 +0100

    fix: remove redundant check and introduce safeParseInt (PR Review #273)

--- recipes/infomaniak-mail/webview.js
+++ recipes/infomaniak-mail/webview.js
@@ -4,18 +4,18 @@ module.exports = Ferdium => {
     const inboxField = document.querySelector('.ws-tree-node-content')
     const inboxCountField = inboxField.querySelector('.ws-tree-node-badge');
     const inboxCountText = inboxCountField ? inboxCountField.textContent : null;
-    const inboxCount = inboxCountField && inboxCountText ? Number(inboxCountText) : 0;
-    
+    const inboxCount = inboxCountText ? Ferdium.safeParseInt(inboxCountText) : 0;
+
     const importantBadge = inboxCount;
     let unimportantBadge = 0;
-    
-    if (inboxCount == 0){
+
+    if (inboxCount === 0){
       // This selects the first folder with an unread message count.
       // The actaul count and the total of all other folders is not needed as the badge has no number.
       const totalCountField = document.querySelector('.ws-tree-node-badge');
       const totalCountText = totalCountField ? totalCountField.textContent : null;
-      const totalCount = totalCountField && totalCountText ? Number(totalCountText) : 0;
-      
+      const totalCount = totalCountText ? Ferdium.safeParseInt(totalCountText) : 0;
+
       unimportantBadge = totalCount;
     }
 
