042838ec4263950c62f5c1c978dbcd0d529719a1
Raphael Jenni <RaphaelJenni@users.noreply.github.com>
2023-01-02 12:10:44 +0100

    Fix: Infomaniak unread messages badge to exclude spam emails (#273)

--- recipes/infomaniak-mail/webview.js
+++ recipes/infomaniak-mail/webview.js
@@ -1,8 +1,22 @@
 module.exports = Ferdium => {
   const getMessages = () => {
-    const count = document.querySelector('.ws-tree-node-badge');
-    const countText = count ? count.textContent : null;
-    Ferdium.setBadge(count && countText ? Number(countText) : 0);
+    // This selects the first folder (the inbox and reads its unread messages count)
+    const inboxField = document.querySelector('.ws-tree-node-content')
+    const inboxCountField = inboxField.querySelector('.ws-tree-node-badge');
+    const inboxCountText = inboxCountField ? inboxCountField.textContent : null;
+    const inboxCount = inboxCountText ? Ferdium.safeParseInt(inboxCountText) : 0;
+
+    let unimportantCount = 0;
+
+    if (inboxCount === 0) {
+      // This selects the first folder with an unread message count.
+      // The actaul count and the total of all other folders is not needed as the badge has no number.
+      const totalCountField = document.querySelector('.ws-tree-node-badge');
+      const totalCountText = totalCountField ? totalCountField.textContent : null;
+      unimportantCount = totalCountText ? Ferdium.safeParseInt(totalCountText) : 0;
+    }
+
+    Ferdium.setBadge(inboxCount, unimportantCount);
   };
 
   Ferdium.loop(getMessages);
