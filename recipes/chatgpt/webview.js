beae45da2f6c9fcb2947ab50c9517fb75c7a854d
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2023-01-16 21:19:26 +0000

    refactor: ternary condition

--- recipes/chatgpt/webview.js
+++ recipes/chatgpt/webview.js
@@ -1,9 +1,5 @@
 module.exports = Ferdium => {
   Ferdium.handleDarkMode(isEnabled => {
-    if (isEnabled) {
-      localStorage.setItem('theme', 'dark');
-    } else {
-      localStorage.setItem('theme', 'light');
-    }
+    localStorage.setItem('theme', isEnabled ? 'dark' : 'light' );
   });
 };
