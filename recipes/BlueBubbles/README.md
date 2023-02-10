d3fe8656e237f54b3513320752c0b7c18936535a
httpsdalia <82799862+httpsdalia@users.noreply.github.com>
2023-02-10 01:30:33 -0500

    Add files via upload

---
+++ recipes/BlueBubbles/README.md
@@ -0,0 +1,2 @@
+# BlueBubbles for ferdium
+This is a ferdium recipe for BlueBubbles
---
+++ recipes/BlueBubbles/webview.js
@@ -0,0 +1,8 @@
+module.exports = () => {
+  // TODO: If your BlueBubbles service has unread messages, uncomment these lines to implement the logic for updating the badges
+  // const getMessages = () => {
+  //   // TODO: Insert your notification-finding code here
+  //   Ferdium.setBadge(0, 0);
+  // };
+  // Ferdium.loop(getMessages);
+};
