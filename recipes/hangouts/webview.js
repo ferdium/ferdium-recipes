0bf43d23b4b0a1f2f93918d44b8c7a2fc0121646
tamas646 <ptamas2002@gmail.com>
2023-03-11 17:17:05 +0100

    Made requested changes

--- recipes/hangouts/webview.js
+++ recipes/hangouts/webview.js
@@ -2,7 +2,7 @@ module.exports = Ferdium => {
   const getMessages = () => {
     // get unread messages
     let count = 0;
-    for (const span of document.querySelectorAll('span[jsname=DW2nlb]'))  count += span.textContent == '' ? 0 : Number.parseInt(span.textContent);
+    for (const span of document.querySelectorAll('span[jsname=DW2nlb]'))  count += Ferdium.safeParseInt(span.textContent);
 
     // set Ferdium badge
     Ferdium.setBadge(count);
