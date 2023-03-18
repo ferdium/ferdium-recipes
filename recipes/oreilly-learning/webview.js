ffefe6d13b1b52c93f3f50d42c59578f00de4f99
Emanuel Fonseca <codeplating@gmail.com>
2023-03-18 11:27:51 +0000

    chore: code cleanup

--- recipes/oreilly-learning/webview.js
+++ recipes/oreilly-learning/webview.js
@@ -5,12 +5,5 @@ function _interopRequireDefault(obj) {
 }
 
 module.exports = Ferdium => {
-  // TODO: If your OReilly Service service has unread messages, uncomment these lines to implement the logic for updating the badges
-  // const getMessages = () => {
-  //   // TODO: Insert your notification-finding code here
-  //   Ferdium.setBadge(0, 0);
-  // };
-  // Ferdium.loop(getMessages);
-
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
