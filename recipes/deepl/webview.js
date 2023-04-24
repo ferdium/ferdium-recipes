a5ce20bd0a6a310b4f77a9d797d20d57927056ff
Alphrag <34252790+Alphrag@users.noreply.github.com>
2023-04-24 22:45:03 +0100

    Add code to support 'service.css'

--- recipes/deepl/webview.js
+++ recipes/deepl/webview.js
@@ -1,1 +1,9 @@
-module.exports = Ferdium => Ferdium;
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+module.exports = Ferdium => {
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
