5808e22ab2459f9540abf6e683b55a60436e5873
Alphrag <34252790+Alphrag@users.noreply.github.com>
2023-04-24 23:37:23 +0100

    Add code to support 'service.css'

--- recipes/gotify/webview.js
+++ recipes/gotify/webview.js
@@ -1,3 +1,9 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
 module.exports = Ferdium => {
   const getMessages = () => {
     let count = document.querySelectorAll('#messages').length;
@@ -6,4 +12,5 @@ module.exports = Ferdium => {
   };
 
   Ferdium.loop(getMessages);
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
