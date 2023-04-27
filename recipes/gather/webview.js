9342c2a0fee3faaa6bd0c8b853b54b09d5b3d864
Victor Bonnelle <victor.bonnelle@protonmail.com>
2023-04-27 13:18:06 +0200

    Use Ferdium.safeParseInt and shorten code in Gather

--- recipes/gather/webview.js
+++ recipes/gather/webview.js
@@ -6,13 +6,8 @@ function _interopRequireDefault(obj) {
 
 module.exports = Ferdium => {
   const getMessages = () => {
-    const unreadCountElement = document.querySelector("[aria-label='Chat'] > div > div > p");
-    if (unreadCountElement !== null) {
-      const unreadCount = unreadCountElement.textContent * 1;
-      Ferdium.setBadge(unreadCount);
-    } else {
-      Ferdium.setBadge(0);
-    }
+    const element = document.querySelector("[aria-label='Chat'] > div > div > p");
+    Ferdium.setBadge(element ? Ferdium.safeParseInt(element.textContent) : 0);
   }
 
   Ferdium.loop(getMessages);
