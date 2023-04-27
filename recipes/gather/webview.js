03cb3805061f126f247c856a4a7daae347946530
Victor Bonnelle <victor.bonnelle@protonmail.com>
2023-04-27 13:00:33 +0200

    Reset unread counter when leaving space in Gather

--- recipes/gather/webview.js
+++ recipes/gather/webview.js
@@ -6,8 +6,13 @@ function _interopRequireDefault(obj) {
 
 module.exports = Ferdium => {
   const getMessages = () => {
-    const unreadCount = document.querySelector("[aria-label='Chat'] > div > div > p").textContent * 1;
-    Ferdium.setBadge(unreadCount);
+    const unreadCountElement = document.querySelector("[aria-label='Chat'] > div > div > p");
+    if (unreadCountElement !== null) {
+      const unreadCount = unreadCountElement.textContent * 1;
+      Ferdium.setBadge(unreadCount);
+    } else {
+      Ferdium.setBadge(0);
+    }
   }
 
   Ferdium.loop(getMessages);
