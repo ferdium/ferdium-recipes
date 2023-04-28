6b852d95793ea0bb9e5c3410b15d7cb1b8644980
Victor B <39555268+victorbnl@users.noreply.github.com>
2023-04-29 00:18:40 +0200

    Fix Google Classroom landing page (#346)

--- recipes/googleclassroom/webview.js
+++ recipes/googleclassroom/webview.js
@@ -1,4 +1,13 @@
 module.exports = Ferdium => {
+  // if the user is on googleclassroom landing page, go to the login page.
+  if (
+    location.hostname == 'edu.google.com' &&
+    location.href.includes('workspace-for-education/classroom/')
+  ) {
+    location.href =
+      'https://accounts.google.com/AccountChooser?continue=https://classroom.google.com/u/0/';
+  }
+
   const getMessages = () => {
     let homework = 0;
     const upcomingAssignment = document.querySelectorAll('.hrUpcomingAssignmentGroup');
