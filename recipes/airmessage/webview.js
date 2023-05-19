dd6c1253b6de5b47e27ce66cc4ab2c9bb868ca39
Clayton <clayton.belcher@gmail.com>
2023-05-18 22:57:20 -0400

    Add badge count support to AirMessage recipe

--- recipes/airmessage/webview.js
+++ recipes/airmessage/webview.js
@@ -1,8 +1,15 @@
-module.exports = () => {
-  // TODO: If your AirMessage service has unread messages, uncomment these lines to implement the logic for updating the badges
-  // const getMessages = () => {
-  //   // TODO: Insert your notification-finding code here
-  //   Ferdium.setBadge(0, 0);
-  // };
-  // Ferdium.loop(getMessages);
+module.exports = (Ferdium) => {
+  const getMessages = function getMessages() {
+    let count = 0;
+    let messageNameElements = document.getElementsByClassName("MuiTypography-body1");
+    if (messageNameElements.length > 0) {
+      for (i = 0; i < messageNameElements.length; i++) {
+        if (getComputedStyle(messageNameElements[i]).getPropertyValue('font-weight') == "700") {
+          count++;
+        }
+      }
+    }
+    Ferdium.setBadge(count);
+  };
+  Ferdium.loop(getMessages);
 };
