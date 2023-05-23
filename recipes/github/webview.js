64478b0545d687d977e1f07d51fb5de57ff6f382
Joseph Hale <me@jhale.dev>
2023-05-22 22:52:25 -0700

    fix(github): Include "new notifications" in direct total
    GitHub's notification page no longer updates the notification
    count when notifications arrive. Instead, it displays a small
    message saying "COUNT new notifications".
    
    This commit includes that COUNT in the number of direct messages
    reported to Ferdium.

--- recipes/github/webview.js
+++ recipes/github/webview.js
@@ -1,11 +1,22 @@
 module.exports = Ferdium => {
+  const _parseNewCount = (text) => {
+    const match = text.match(/\d+/);
+    return match ? Ferdium.safeParseInt(match[0]) : 0;
+  };
+
   const getMessages = () => {
     const directCountElement = document.querySelector(
       '.filter-list.js-notification-inboxes .count',
     );
-    const directCount = directCountElement
+    let directCount = directCountElement
       ? Ferdium.safeParseInt(directCountElement.textContent)
       : 0;
+    
+    const newCountElement = document.querySelector('a.h6[href="/notifications?query="]');
+    const newCount = newCountElement ?
+      _parseNewCount(newCountElement.textContent) : 0;
+    directCount += newCount;
+
     const indirectCount = document.querySelector(
       '[class*="mail-status unread"]:not([hidden])',
     ) ? 1 : 0;
