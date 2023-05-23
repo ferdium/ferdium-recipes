2c3b62366156bfa6ab799abeb3054154e6eb6bc0
Joseph Hale <me@jhale.dev>
2023-05-22 22:23:28 -0700

    feat(tt-rss): Limit notifications to a Category.
    As described in the updated README, users can now choose to limit
    notifications to only new articles published under a Category of
    their choosing. Articles published in other Categories are then
    considered "indirect" messages.
    
    This feature is make available via parsing a custom URL parameter
    from the Service URL. Preferrably Ferdium would enable recipies to
    request custom fields from users, but for now this workaround
    accomplishes the task.

--- recipes/tt-rss/webview.js
+++ recipes/tt-rss/webview.js
@@ -1,16 +1,41 @@
 "use strict";
 
 module.exports = Ferdium => {
+  const _notificationCategory = new URL(window.location.href).searchParams.get("ferdiumNotificationCategory");
+  
   const getMessages = function getMessages() {
-    // Initialize empty vars
-    var unread = 0;
-    var match = [];
-    // Extract the number from the title
-    match = document.title.match(/^\((\d+)\) Tiny Tiny RSS$/);
-    // Some logic to handle the match groups
-    unread = match != null && match.length > 0 ? match[1] : 0;
-    // Set unread msgs badge
-    Ferdium.setBadge(Number.parseInt(unread, 10));
+    if (_notificationCategory) {
+      countMessagesUnderCategory(_notificationCategory);
+    } else {
+      countMessages();
+    }
+  };
+
+  const countMessagesUnderCategory = (notificationCategory) => {
+    var elements = document.querySelectorAll(".dijitTreeIsRoot")
+    var directMessages = 0;
+    var indirectMessages = 0;
+    for (var element of elements) {
+      var label = element.querySelectorAll(".dijitTreeLabel")[0];
+      var unreadNode = element.querySelectorAll(".unread")[0];
+      var unreadAmount = Ferdium.safeParseInt(unreadNode.textContent);
+      if (label.textContent === notificationCategory) {
+        directMessages += unreadAmount;
+      } else {
+        indirectMessages += unreadAmount;
+      }
+    }
+    Ferdium.setBadge(directMessages, indirectMessages);
+  };
+
+  const _countMessagesFromTitle = () => {
+    var match = document.title.match(/^\((\d+)\) Tiny Tiny RSS$/);
+    var count = match != null && match.length > 0 ? match[1] : 0;
+    return Ferdium.safeParseInt(count);
+  }
+
+  const countMessages = () => {
+    Ferdium.setBadge(_countMessagesFromTitle());
   };
 
   const loopFunc = () => {
