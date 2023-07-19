d028830bc9e474115973bdc79fbac150bce054aa
Victor B <39555268+victorbnl@users.noreply.github.com>
2023-07-20 01:32:47 +0200

    Add Skiff Mail recipe (#391)

---
+++ recipes/skiff-mail/webview.js
@@ -0,0 +1,17 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+module.exports = Ferdium => {
+  const getMessages = () => {
+    const element = document.querySelector('div[href="/mail/inbox"]');
+    const matches = element.textContent.match(/\d+/);
+    const unreadCount = Ferdium.safeParseInt(matches ? matches[0] : 0);
+    Ferdium.setBadge(unreadCount);
+  };
+  Ferdium.loop(getMessages);
+
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
