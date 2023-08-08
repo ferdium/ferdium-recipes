23bf6499abf5b9d002deff1cf7aa4dfd6d08450f
MCMXC <16797721+mcmxcdev@users.noreply.github.com>
2023-08-07 19:31:13 -0600

    use Ferdium.handleDarkMode() in google-calendar webview to be able to toggle between light and dark mode

--- recipes/google-calendar/webview.js
+++ recipes/google-calendar/webview.js
@@ -7,12 +7,13 @@ const _path = _interopRequireDefault(require('path'));
 module.exports = Ferdium => {
   // if the user is on googlecalendar landing page, go to the login page.
   if (
-    location.hostname == 'workspace.google.com' &&
+    location.hostname === 'workspace.google.com' &&
     location.href.includes('products/calendar/')
   ) {
     location.href =
       'https://accounts.google.com/AccountChooser?continue=https://calendar.google.com/u/0/';
   }
+
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
   Ferdium.injectCSS(
     'https://cdn.statically.io/gh/ferdium/ferdium-recipes/main/recipes/google-calendar/calendar.css',
@@ -21,17 +22,24 @@ module.exports = Ferdium => {
     'https://cdn.statically.io/gh/ferdium/ferdium-recipes/main/recipes/google-calendar/webview-unsave.js',
   );
 
-  // Workaround for loading darkmode.css
-  const cssId = 'cssDarkModeWorkaround';
-  if (!document.querySelector(cssId)) {
-    const head = document.querySelectorAll('head')[0];
-    const link = document.createElement('link');
-    link.id = cssId;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href =
-      'https://cdn.statically.io/gh/ferdium/ferdium-recipes/main/recipes/google-calendar/darkmode.css';
-    link.media = 'all';
-    head.append(link);
-  }
+  Ferdium.handleDarkMode(isEnabled => {
+    const cssId = 'cssDarkModeWorkaround';
+
+    if (isEnabled) {
+      // Workaround for loading darkmode.css
+      if (!document.querySelector(`#${cssId}`)) {
+        const head = document.querySelectorAll('head')[0];
+        const link = document.createElement('link');
+        link.id = cssId;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href =
+          'https://cdn.statically.io/gh/ferdium/ferdium-recipes/main/recipes/google-calendar/darkmode.css';
+        link.media = 'all';
+        head.append(link);
+      }
+    } else {
+      document.querySelector(`#${cssId}`)?.remove();
+    }
+  });
 };
