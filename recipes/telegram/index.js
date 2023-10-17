733c37922321c0cfb7a63c23e105a316dc4d0f1b
Nikolay Khodov <i@valt.me>
2023-10-18 01:00:05 +0200

    Fix message notifications in Telegram (#439)
    * Fix message notifications in Telegram
    
    * Update index.js
    
    * Bump version to 3.4.4
    
    * Update namespace
    
    * Fix lint issue
    
    * Fix lint errors

--- recipes/telegram/index.js
+++ recipes/telegram/index.js
@@ -1,1 +1,24 @@
-module.exports = Ferdium => Ferdium;
+function instrumenEnvironment(webview) {
+  webview.executeJavaScript(`
+    (function() {
+        if(window.electron) {
+            return;
+         }
+        
+        window.electron = { };
+    })();
+  `);
+}
+module.exports = Ferdium =>
+  class Telegram extends Ferdium {
+    // https://www.electronjs.org/docs/latest/api/webview-tag/#dom-events
+    get events() {
+      return {
+        'load-commit': 'loadCommit',
+      };
+    }
+
+    loadCommit(event) {
+      instrumenEnvironment(event.target);
+    }
+  };
