ce35785f22c2e8e3640b29a2a160cd3f52a0290b
Edgars <Edgars+GitHub@gaitenis.id.lv>
2023-11-24 05:09:53 +0000

    Add unread chats count to Zoho Mail's badge

--- recipes/zoho/webview-unsafe.js
+++ recipes/zoho/webview-unsafe.js
@@ -1,7 +1,10 @@
 // Wait for Ferdium to initialize
 if (window.ferdium?.setBadge !== undefined) {
   window.ferdium.setBadge(
-    window.ferdium.safeParseInt(window.zmfolAction?.getUnreadViewCount()),
+    window.ferdium.safeParseInt(window.zmfolAction?.getUnreadViewCount()) +
+      window.ferdium.safeParseInt(
+        document.querySelector('#wms_menu_unreadchats_cnt')?.textContent,
+      ),
     window.ferdium.safeParseInt(
       window.zmTopBar?.topBandElements()?.notification?.children
         ?.notificationBadge?.textContent,
