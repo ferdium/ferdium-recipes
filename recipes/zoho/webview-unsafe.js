44d11975596afa72f1ffb67816c40bfd734a8262
Edgars <eandersons@users.noreply.github.com>
2023-11-25 20:41:24 +0200

    Fix Zoho Mail badge (#466)

--- recipes/zoho/webview-unsafe.js
+++ recipes/zoho/webview-unsafe.js
@@ -1,12 +1,13 @@
-// wait for Ferdium and Zoho Mail to initialize
-if (
-  Object.prototype.hasOwnProperty.call(window, 'ferdium') &&
-  Object.prototype.hasOwnProperty.call(window.ferdium, 'setBadge') &&
-  Object.prototype.hasOwnProperty.call(window, 'zmNCenter') &&
-  Object.prototype.hasOwnProperty.call(window, 'zmfolAction')
-) {
-  const unreadNotifications = window.zmNCenter.counter.count(); // General Notifications by Zoho (Bell Icon)
-  const unreadMail = window.zmfolAction.getUnreadViewCount(); // Unread messages count
-
-  window.ferdium.setBadge(unreadMail, unreadNotifications);
+// Wait for Ferdium to initialize
+if (window.ferdium?.setBadge !== undefined) {
+  window.ferdium.setBadge(
+    window.ferdium.safeParseInt(window.zmfolAction?.getUnreadViewCount()) +
+      window.ferdium.safeParseInt(
+        document.querySelector('#wms_menu_unreadchats_cnt')?.textContent,
+      ),
+    window.ferdium.safeParseInt(
+      window.zmTopBar?.topBandElements()?.notification?.children
+        ?.notificationBadge?.textContent,
+    ),
+  );
 }
