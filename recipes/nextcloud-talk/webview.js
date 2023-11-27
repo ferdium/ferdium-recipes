19a4ccc4bad9a0482c5e92ef5c233e3f8b387966
Edgars <Edgars+GitHub@gaitenis.id.lv>
2023-11-27 10:56:01 +0200

    Fix selectors for Nextcloud Talk
    Script and CSS was updated to add selectors for the latest Nextcloud
    (27).
    Previous selectors were left as is for backwards compatibility.
    
    Additionally direct notifications count now is detected by unread
    conversations count in the left sidebar, using Talk specific
    notifications as a fallback.
    
    Fixes ferdium/ferdium-app#971

--- recipes/nextcloud-talk/webview.js
+++ recipes/nextcloud-talk/webview.js
@@ -6,38 +6,46 @@ const _path = _interopRequireDefault(require('path'));
 
 module.exports = Ferdium => {
   const getMessages = () => {
-    let direct = 0;
-
-    const notificationWrapper = document.querySelector(
-      '.notifications .notification-wrapper',
-    );
-
-    if (notificationWrapper) {
-      const directSelector = notificationWrapper.querySelectorAll(
-        '.notification[object_type="chat"], .notification[object_type="room"]',
-      );
-      direct = directSelector ? Ferdium.safeParseInt(directSelector.length) : 0;
-    }
-
+    // With `// Legacy ` are marked those selectors that were working for some
+    // Nextcloud version before 27 (24 or 25).
+    const counterBubble = '.counter-bubble__counter';
+    const directFromLeftSideBar = document.querySelectorAll(
+      `${counterBubble}--highlighted`, // Nextcloud 27
+    ).length;
     let indirect = 0;
-
     for (const counter of document.querySelectorAll(
-      '.app-navigation-entry__counter',
+      '.app-navigation-entry__counter, ' + // Legacy
+        `${counterBubble}:not(${counterBubble}--highlighted)`, // Nextcloud 27
     )) {
-      const entryCounter = counter
-        ? Ferdium.safeParseInt(counter.textContent)
-        : 0;
-      indirect += entryCounter;
+      indirect += Ferdium.safeParseInt(counter?.textContent.trim());
     }
 
     if (document.title.startsWith('*')) {
       indirect += 1;
     }
 
-    Ferdium.setBadge(direct, indirect);
+    Ferdium.setBadge(
+      // Try to use the unread conversations count retrieved from the left
+      // sidebar, otherwise check Talk specific notifications
+      directFromLeftSideBar > 0
+        ? directFromLeftSideBar
+        : Ferdium.safeParseInt(
+            document
+              .querySelector(
+                '.notifications .notification-wrapper, ' + // Legacy
+                  '.notification-container .notification-wrapper', // Nextcloud 27
+              )
+              ?.querySelectorAll(
+                '.notification[object_type="chat"], ' + // Legacy
+                  '.notification[object_type="room"], ' + // Legacy
+                  '.notification[data-object-type="chat"], ' + // Nextcloud 27
+                  '.notification[data-object-type="room"]', // Nextcloud 27
+              ).length,
+          ),
+      indirect,
+    );
   };
 
   Ferdium.loop(getMessages);
-
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
