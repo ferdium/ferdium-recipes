c061e152716c8101c8c201a7a17c1cfee816ec0a
rohansx <sharmarohan615@gmail.com>
2024-01-03 15:36:03 +0530

    add new recipe-hackernews

---
+++ recipes/hackernews/webview.js
@@ -0,0 +1,20 @@
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+const _path = _interopRequireDefault(require('path'));
+
+module.exports = Ferdium => {
+  const getMessages = () => {
+    // get unread messages
+    const count = document.querySelectorAll('.guilds-wrapper .badge').length;
+
+    // set Ferdium badge
+    Ferdium.setBadge(count);
+  };
+
+  Ferdium.loop(getMessages);
+
+  // Hide download message
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
