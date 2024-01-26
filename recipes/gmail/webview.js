2b0b77ab13f457b51cb0f3dc5602ffea1abc6801
MikeDabrowski <mike2060@gmail.com>
2024-01-26 07:42:19 +0000

    fix: gmail notifications doubled

--- recipes/gmail/webview.js
+++ recipes/gmail/webview.js
@@ -15,47 +15,14 @@ module.exports = Ferdium => {
   }
 
   const getMessages = () => {
-    let countImportant = 0;
-    let countNonImportant = 0;
-    const inboxLinks = document.querySelectorAll('.J-Ke.n0');
-    const spaceAndChatBadges = document.querySelectorAll(
-      'div.Xa.bSyoAf span.XU',
+    const mailChatMeetBadges = document.querySelectorAll('span.XU.aH6');
+
+    const mailChatMeetCounts = [...mailChatMeetBadges].reduce(
+      (acc, e) => Ferdium.safeParseInt(e.getInnerHTML()) + acc,
+      0,
     );
 
-    if (inboxLinks.length > 0) {
-      const { parentNode } = inboxLinks[0];
-      if (parentNode) {
-        const parentNodeOfParentNode = parentNode.parentNode;
-        if (parentNodeOfParentNode) {
-          const unreadCounts = parentNodeOfParentNode.querySelectorAll('.bsU');
-          if (unreadCounts.length > 0) {
-            const unreadCount = unreadCounts[0].textContent;
-            if (unreadCount.includes(':')) {
-              const counts = unreadCount
-                .split(':')
-                .map(s => Ferdium.safeParseInt(s.replaceAll(/[^\p{N}]/gu, '')));
-              countImportant = counts[0];
-              countNonImportant = counts[1] - counts[0];
-            } else {
-              countImportant = Ferdium.safeParseInt(
-                unreadCount.replaceAll(/[^\p{N}]/gu, ''),
-              );
-            }
-          }
-        }
-      }
-    }
-
-    if (spaceAndChatBadges.length > 0) {
-      const arr = [...spaceAndChatBadges];
-      const spaceAndChatCount = arr.reduce(
-        (acc, e) => Ferdium.safeParseInt(e.getInnerHTML()) + acc,
-        0,
-      );
-      countImportant += spaceAndChatCount;
-    }
-
-    Ferdium.setBadge(countImportant, countNonImportant);
+    Ferdium.setBadge(mailChatMeetCounts);
   };
 
   Ferdium.loop(getMessages);
