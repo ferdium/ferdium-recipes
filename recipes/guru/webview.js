bf88eba2a32cbf10c463ec9fbafa530e46e73559
sulthankarimov <sulthankarimov@gmail.com>
2024-05-09 07:39:58 +0700

    resurrect support for injecting service.css

--- recipes/guru/webview.js
+++ recipes/guru/webview.js
@@ -1,4 +1,8 @@
-//const path = require('path');
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+const _path = _interopRequireDefault(require('path'));
 
 module.exports = Ferdium => {
   const getMessages = () => {
@@ -10,4 +14,6 @@ module.exports = Ferdium => {
   };
 
   Ferdium.loop(getMessages);
+
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
