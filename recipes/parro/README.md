4567efb02e03fce87315ec9d9df77521294ac144
Richard Beumer <richard@wilmar.nl>
2024-07-04 17:16:33 +0200

    fix .css; remove readme.md

--- recipes/parro/README.md
+++
@@ -1,7 +0,0 @@
-# Parro for Ferdium
-This is the official Ferdium recipe for Parro. 
-
-Parro is a dutch communication application for parents to communicate with their kids teachers.
-
-### How to create your own Ferdium recipes:
-* [Read the documentation](https://github.com/ferdium/ferdium-recipes/blob/main/docs/integration.md)
--- recipes/parro/webview.js
+++ recipes/parro/webview.js
@@ -1,3 +1,9 @@
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+const _path = _interopRequireDefault(require('path'));
+
 module.exports = Ferdium => {
   function getMessages() {
     let direct = 0;
@@ -11,5 +17,6 @@ module.exports = Ferdium => {
     Ferdium.setBadge(direct, indirect);
   }
 
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
   Ferdium.loop(getMessages);
 };
