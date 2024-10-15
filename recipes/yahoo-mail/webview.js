35bd6e426dd8596bb7eefad416ba7e542205e329
Ashish Dobani <dobaniashish@yahoo.com>
2024-10-15 15:47:46 +0530

    fix(yahoo-mail): update badge to 0 when all mail is read (#599)

--- recipes/yahoo-mail/webview.js
+++ recipes/yahoo-mail/webview.js
@@ -6,9 +6,11 @@ const _path = _interopRequireDefault(require('path'));
 
 module.exports = Ferdium => {
   const getMessages = () => {
-    const count = document.querySelector(
-      'a[data-test-folder-name="Inbox"] span[data-test-id="displayed-count"]',
-    ).textContent;
+    const count = Ferdium.safeParseInt(
+      document.querySelector(
+        'a[data-test-folder-name="Inbox"] span[data-test-id="displayed-count"]',
+      )?.textContent,
+    );
     Ferdium.setBadge(count);
   };
 
