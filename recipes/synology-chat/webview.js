dee6ba08afbe7f3e0fbb42628e6979a696b9fb62
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2025-03-11 11:08:04 +0000

    Add badge logic to service (ferdium/ferdium-app#2053)

--- recipes/synology-chat/webview.js
+++ recipes/synology-chat/webview.js
@@ -1,7 +1,6 @@
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
 }
-
 const _path = _interopRequireDefault(require('path'));
 
 module.exports = Ferdium => {
@@ -10,4 +9,17 @@ module.exports = Ferdium => {
   });
 
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+
+  Ferdium.loop(() => {
+    const unreadElements = document.querySelectorAll(
+      '.highlight, .highlight-mention',
+    );
+    let unreadCount = 0;
+
+    if (unreadElements.length > 0) {
+      unreadCount = unreadElements.length;
+    }
+
+    Ferdium.setBadge(unreadCount);
+  });
 };
