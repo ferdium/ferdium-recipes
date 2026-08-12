d42853f06c9ccb41c1d4894ba01a5bd1b7f6b2ab
maki <maki@hotmilk.space>
2026-08-12 14:10:59 -0400

    Improve miniflux recipe

--- recipes/miniflux/webview.js
+++ recipes/miniflux/webview.js
@@ -5,5 +5,37 @@ function _interopRequireDefault(obj) {
 const _path = _interopRequireDefault(require('path'));
 
 module.exports = Ferdium => {
+  const getMessages = () => {
+    const unreadEl = document.querySelector('a[data-page="unread"]');
+    if (unreadEl == null) return Ferdium.setBadge(0);
+
+    const matches = unreadEl.textContent.match(/Unread\s+\((\d+)\)/);
+    if (matches == null) return Ferdium.setBadge(0);
+
+    Ferdium.setBadge(Number(matches[1]));
+  };
+
+  Ferdium.loop(getMessages);
+
   Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+
+  // only reload page if not reading an entry or in settings
+
+  if (
+    window.location.pathname.includes('/entry/') ||
+    window.location.pathname == '/settings'
+  ) {
+    return;
+  }
+
+  const refreshMinutes = 5;
+
+  // console.log("after "+refreshMinutes+" minutes")
+
+  setTimeout(
+    () => {
+      window.location.reload();
+    },
+    1000 * 60 * refreshMinutes,
+  );
 };
