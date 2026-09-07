f5edeaa9b78f7467f30416d0b47f294d43f7a8e9
0xCmdrKeen <98132670+0xCmdrKeen@users.noreply.github.com>
2026-09-07 01:17:11 -0700

    Add recipe for Primal (#740)
    * Add Primal recipe
    
    * Fix formatting

---
+++ recipes/primal/webview.js
@@ -0,0 +1,33 @@
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+const _path = _interopRequireDefault(require('path'));
+
+module.exports = Ferdium => {
+  Ferdium.handleDarkMode(isEnabled => {
+    localStorage.setItem('theme', isEnabled ? 'midnight' : 'ice');
+  });
+
+  const getMessages = () => {
+    let direct = 0;
+
+    // "Notifications" and "Messages". Sum the values for direct badge.
+    const notificationsElement = document.querySelector(
+      'a[href="/notifications"]',
+    ).nextSibling;
+    if (notificationsElement) {
+      direct += Ferdium.safeParseInt(notificationsElement.textContent);
+    }
+    const DMElement = document.querySelector('a[href="/dms"]').nextSibling;
+    if (DMElement) {
+      direct += Ferdium.safeParseInt(DMElement.textContent);
+    }
+
+    Ferdium.setBadge(direct);
+  };
+
+  Ferdium.loop(getMessages);
+
+  Ferdium.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
