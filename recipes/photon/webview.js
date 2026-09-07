bc708f6aad02fc43168e976d1d65678eaf96f21b
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2026-09-07 13:23:28 +0100

    Fix photon reformat

--- recipes/photon/webview.js
+++ recipes/photon/webview.js
@@ -4,7 +4,7 @@ function _interopRequireDefault(obj) {
 
 const _path = _interopRequireDefault(require('path'));
 
-module.exports = (Ferdium) => {
+module.exports = Ferdium => {
   let count = 0;
 
   const getMessages = async () => {
@@ -15,16 +15,18 @@ module.exports = (Ferdium) => {
 
     // read active profile info from LocalStorage
     const profileData = localStorage.getItem('profileData');
-    const {profile, profiles} = JSON.parse(profileData);
-    const {instance, jwt} = profiles.find(({id}) => id === profile);
+    const { profile, profiles } = JSON.parse(profileData);
+    const { instance, jwt } = profiles.find(({ id }) => id === profile);
 
     // obtain unread count from user instance
     const response = await fetch(
-      `https://${instance}/api/v3/user/unread_count`, {
-      headers: {
-        authorization: `Bearer ${jwt}`
-      }
-    });
+      `https://${instance}/api/v3/user/unread_count`,
+      {
+        headers: {
+          authorization: `Bearer ${jwt}`,
+        },
+      },
+    );
 
     // parse response
     const { replies, mentions, private_messages } = await response.json();
